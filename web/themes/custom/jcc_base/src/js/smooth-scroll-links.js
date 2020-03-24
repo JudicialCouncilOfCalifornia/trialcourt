8cc796b9e244650382935e8f15f8cdb748e6fd37
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2020-03-23 17:33:15 -0700

    Remove SLO, merge changes to jcc_base.  Move jcc_base to custom.

---
+++ web/themes/custom/jcc_base/src/js/smooth-scroll-links.js
@@ -0,0 +1,21 @@
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.smoothScrollLinks = {
+    attach: function (context) {
+      $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 100
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    }
+  }
+})(jQuery, Drupal);
