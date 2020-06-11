993f9d0d33084eb0190811cf7767e5b82717d786
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2020-06-11 16:18:18 -0700

    TC-264: update CircleCI tooling to deploy to multiple pantheon environments.

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -2,13 +2,22 @@
 
 // Develop.
 $sites['develop-jcc-tc.pantheonsite.io'] = 'slo';
+$sites['develop-jcc-oc.pantheonsite.io'] = 'oc';
 
 // Stage.
 $sites['stage-jcc-tc.pantheonsite.io'] = 'slo';
+$sites['stage-jcc-oc.pantheonsite.io'] = 'oc';
 
 // Live.
+$sites['dev-jcc-tc.pantheonsite.io'] = 'slo';
+$sites['test-jcc-tc.pantheonsite.io'] = 'slo';
 $sites['live-jcc-tc.pantheonsite.io'] = 'slo';
 $sites['www.slo.courts.ca.gov'] = 'slo';
+
+$sites['dev-jcc-oc.pantheonsite.io'] = 'oc';
+$sites['test-jcc-oc.pantheonsite.io'] = 'oc';
+$sites['live-jcc-oc.pantheonsite.io'] = 'oc';
+$sites['www.occourts.org'] = 'oc';
 
 // Local using Other.
 // If you're not using Lando, place additional site definitions in
@@ -21,6 +30,7 @@ if (file_exists($app_root . '/sites/sites.local.php')) {
 $sites['epic-multi-jcc-tc.pantheonsite.io'] = 'default';
 $sites['epic-slo-jcc-tc.pantheonsite.io'] = 'slo';
 $sites['epic-oc-jcc-tc.pantheonsite.io'] = 'oc';
+$sites['epic-multi-jcc-oc.pantheonsite.io'] = 'oc';
 
 // Local using Lando.
 // This list is at the end as it may be automatically ammended by installer.
