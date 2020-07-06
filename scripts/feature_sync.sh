90e3205dde6bbff55851a11e20ce97729f66c2ab
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2020-07-06 12:30:59 -0700

    Slo -> Features -> OC all in clean state. Add cache reset to feature_sync.sh

--- scripts/feature_sync.sh
+++ scripts/feature_sync.sh
@@ -17,14 +17,17 @@ SUB_SITES+=('@local.newsroom')
 
 # Export any updated features from source.
 echo -e "\n${G}Exporting feature changes from $SOURCE_SITE${RE}"
+drush $SOURCE_SITE cr
 drush $SOURCE_SITE fu -y --bundle=jcc_tc
 
 # Import feature changes to target or all sub sites.
 if [ "$TARGET_SITE" ] ; then
+  drush @local.$TARGET_SITE cr
   echo -e "${G}\nImporting feature changes to $TARGET_SITE${RE}"
   drush @local.$TARGET_SITE fra -y --bundle=jcc_tc
   echo -e "\nExporting config for @local.${TARGET_SITE}..."
   drush @local.$TARGET_SITE cex -y
+  drush @local.$TARGET_SITE cr
 else
   echo -e "\n${G}Importing feature changes to all subsites.${RE}"
   echo -e "\n${Y}The following sites must be installed locally with features enabled.${RE}"
@@ -33,9 +36,11 @@ else
   done
 
   for SITE in ${SUB_SITES[@]} ; do
+    drush @local.$TARGET_SITE cr
     echo -e "\nUpdating $SITE..."
     drush $SITE fra -y --bundle=jcc_tc
     echo -e "\nExporting config for $SITE..."
     drush $SITE cex -y
+    drush @local.$TARGET_SITE cr
   done
 fi
