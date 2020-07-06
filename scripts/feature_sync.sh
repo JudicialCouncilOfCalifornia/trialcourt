e2a0e11feb33e3f46a940a45c95e8deff789b426
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2020-07-06 09:06:42 -0700

    TC-292: Update install profile, multisite installer, and features to work out the breaks in site generation and feature sync.

--- scripts/feature_sync.sh
+++ scripts/feature_sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+# set -e
 
 R="\033[31m"
 G="\033[32m"
@@ -22,7 +22,9 @@ drush $SOURCE_SITE fu -y --bundle=jcc_tc
 # Import feature changes to target or all sub sites.
 if [ "$TARGET_SITE" ] ; then
   echo -e "${G}\nImporting feature changes to $TARGET_SITE${RE}"
-  drush $TARGET_SITE fra -y --bundle=jcc_tc
+  drush @local.$TARGET_SITE fra -y --bundle=jcc_tc
+  echo -e "\nExporting config for @local.$T{ARGET_SITE}..."
+  drush @local.$TARGET_SITE cex -y
 else
   echo -e "\n${G}Importing feature changes to all subsites.${RE}"
   echo -e "\n${Y}The following sites must be installed locally with features enabled.${RE}"
@@ -33,5 +35,7 @@ else
   for SITE in ${SUB_SITES[@]} ; do
     echo -e "\nUpdating $SITE..."
     drush $SITE fra -y --bundle=jcc_tc
+    echo -e "\nExporting config for $SITE..."
+    drush $SITE cex -y
   done
 fi
