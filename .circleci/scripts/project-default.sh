7333acebd112b61e824e59712cfdd4a61d24c171
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2020-07-09 08:49:29 -0700

    TC-292: update uuid for pantheon default site due to destroy and rebuild. (Pantheon issues on initial attempt).

--- .circleci/scripts/project-default.sh
+++ .circleci/scripts/project-default.sh
@@ -1,5 +1,5 @@
 # Pantheon Project UUID.
-UUID=6099ad1f-879a-40aa-abbe-2b4a5a675ec7
+UUID=9c57991e-d34c-46f8-9ced-99a0c9fe9959
 SITE_CODE=default
 # If false, pantheon will deploy to dev. If true it will get the Live tag.
 LIVE=false
