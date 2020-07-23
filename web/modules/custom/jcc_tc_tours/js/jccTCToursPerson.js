22232ee52353cf554908dba3c35ce34f69dd7d68
Ivan Bueno <ivan.bueno@jud.ca.gov>
2020-07-23 13:38:51 -0700

    WSGEN-945 #comment add contact fields to person

--- web/modules/custom/jcc_tc_tours/js/jccTCToursPerson.js
+++ web/modules/custom/jcc_tc_tours/js/jccTCToursPerson.js
@@ -3,15 +3,15 @@
     attach: function (context, settings) {
 
       $(document).on("click", ".tip-person-body .joyride-next-tip", function() {
-        $('.vertical-tabs__menu-item:eq(1) a').click();
-      });
-
-      $(document).on("click", ".tip-person-media .joyride-next-tip", function() {
         $('.vertical-tabs__menu-item:eq(2) a').click();
       });
 
-      $(document).on("click", ".tip-person-secondary-content .joyride-next-tip", function() {
+      $(document).on("click", ".tip-person-media .joyride-next-tip", function() {
         $('.vertical-tabs__menu-item:eq(3) a').click();
+      });
+
+      $(document).on("click", ".tip-person-secondary-content .joyride-next-tip", function() {
+        $('.vertical-tabs__menu-item:eq(4) a').click();
       });
 
       $(document).on("click", ".tip-person-metadata .joyride-next-tip", function() {
