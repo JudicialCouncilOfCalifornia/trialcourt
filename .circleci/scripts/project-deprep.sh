799aa2bf08091e9be87946622db74b8c710676a9
Ivan Bueno <ivan.bueno@jud.ca.gov>
2020-07-27 18:26:41 -0700

    deprep pantheon id

--- .circleci/scripts/project-deprep.sh
+++ .circleci/scripts/project-deprep.sh
@@ -1,5 +1,5 @@
 # Pantheon Project UUID.
-UUID=project-newsroom.sh
+UUID=47ffed5c-09ba-48f1-951f-09be642c6962
 SITE_CODE=deprep
 # If false, pantheon will deploy to dev. If true it will get the Live tag.
 LIVE=true
