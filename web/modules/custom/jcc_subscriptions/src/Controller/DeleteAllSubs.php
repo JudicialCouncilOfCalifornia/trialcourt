5a7ebda10d1b93bd190d0544ca4f36a85e240e20
baptisten <baptiste.niviere@gmail.com>
2020-08-21 14:53:20 -0700

    added confirmation page for opt out options

--- web/modules/custom/jcc_subscriptions/src/Controller/DeleteAllSubs.php
+++ web/modules/custom/jcc_subscriptions/src/Controller/DeleteAllSubs.php
@@ -3,7 +3,6 @@
 namespace Drupal\jcc_subscriptions\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use MarkRoland\Emma\Client;
 
 /**
  * Deletes all groups from user.
@@ -13,13 +12,14 @@ class DeleteAllSubs extends ControllerBase {
   /**
    * Returns a render-able array.
    */
-  public function content(string $member_id = '') {
-    $emma_config = \Drupal::config('webform_myemma.settings');
-    $emma = new Client($emma_config->get('account_id'), $emma_config->get('public_key'), $emma_config->get('private_key'));
-    $emma->remove_member_from_all_groups($member_id);
-    $user = $emma->get_member_detail($member_id);
+  public function delete(string $member_id = '') {
     $build = [
-      '#markup' => '<div class="jcc-text-section-aside__container jcc-text-section-aside-secondary__container"><div class="body"></p>' . $user->email . ' ' . $this->t('has been removed from all communications.') . '</p></div></div>',
+      '#markup' => '
+        <div class="jcc-text-section-aside__container jcc-text-section-aside-secondary__container">
+            <div class="body">
+                <p><a class="usa-button usa-button--primary" href="/subscriptions/' . $member_id . '/delete-all/confirmed">' . $this->t("Unsubscribe from all communications.") . '</a></p>
+            </div>
+        </div>',
     ];
 
     return $build;
