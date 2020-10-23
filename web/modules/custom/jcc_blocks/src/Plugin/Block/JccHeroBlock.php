7b302b373d2bd395f24e8affbfd453bb0b900bbf
Mel Wong <mel.wong@jud.ca.gov>
2020-10-23 11:50:16 -0700

    WSGEN-1126: remove icon selection experiment

--- web/modules/custom/jcc_blocks/src/Plugin/Block/JccHeroBlock.php
+++ web/modules/custom/jcc_blocks/src/Plugin/Block/JccHeroBlock.php
@@ -224,12 +224,7 @@ class JccHeroBlock extends BlockBase implements ContainerFactoryPluginInterface 
           ['uuid' => $item->link->getDerivativeId()]
         );
       $entity = array_pop($menu_item_extra);
-      $icon = NULL;
-      if (self::getMediaUrl($entity->get('field_icon')->entity)) {
-        $icon = self::getMediaUrl($entity->get('field_icon')->entity);
-      } else if ($entity->field_pattern_library_icon->value) {
-        $icon = $entity->field_pattern_library_icon->value;
-      }
+      $icon = self::getMediaUrl($entity->get('field_icon')->entity);
 
       $links[$index < 4 ? 'icons' : 'buttons'][] = [
         'title' => $item->link->getTitle(),
