e62b20fb66c22d04712ba4a549b7927f122d725e
Ivan Bueno <ivan.bueno@jud.ca.gov>
2020-11-03 16:57:23 -0800

    WSGEN-1146 url shortener for migration

---
+++ web/modules/custom/jcc_tc_migration/src/Plugin/migrate/process/UrlShortener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\jcc_tc_migration\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Drupal\redirect\Entity\Redirect;
+
+/**
+ * Provides url_shortener plugin.
+ *
+ * Returns a shortened URL redirect.
+ *
+ * Usage:
+ *
+ * @code
+ * process:
+ *   bar:
+ *     plugin: url_shortener
+ *     source: url
+ * @endcode
+ *
+ * @MigrateProcessPlugin(
+ *   id = "url_shortener"
+ * )
+ *
+ */
+class UrlShortener extends ProcessPluginBase {
+
+  /**
+  * {@inheritdoc}
+  */
+  public function transform($value, MigrateExecutableInterface $migrateExecutable, Row $row, $destinationProperty) {
+    if (empty($value) || strlen($value) < 100) {
+      return $value;
+    }
+
+    $source = '/r/' . user_password(20);
+    $redirect = Redirect::create();
+    $redirect->setStatusCode('301');
+    $redirect->setLanguage('und');
+    $redirect->setSource($source);
+    $redirect->setRedirect($value);
+    $redirect->save();
+
+    return $source;
+  }
+}
