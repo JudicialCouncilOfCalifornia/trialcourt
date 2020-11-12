1a1ccb65852d4c4bf993d22e456710f354f42fc1
baptisten <baptiste.niviere@gmail.com>
2020-11-12 11:03:50 -0700

    refactoring

--- web/themes/custom/jcc_newsroom/feed-widget.js
+++ web/themes/custom/jcc_newsroom/feed-widget.js
@@ -6,7 +6,8 @@ function jsonCallback(jsonData){
   var mainContainer = document.getElementById('jcc_embed_' + embedArg);
   for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-    div.innerHTML = '<h3>' + data[i].title + '</h3><p>' + data[i].content_html + '</p><p><a target="_blank" href="' + originUrl + '/node/' + data[i].id + '">Read Full Article</a></p>';
+    div.className = "jcc-news-element";
+    div.innerHTML = '<div class="jcc-element-date">' + data[i].date_published + '</div><div class="jcc-element-title"><a href="' + originUrl + '/node/' + data[i].id + '">' + data[i].title + '</a></div><p class="jcc-element-description">' + data[i].content_html + '</p><p class="jcc-element-read-more"><a target="_blank" href="' + originUrl + '/node/' + data[i].id + '">Read Full Article</a></p>';
     mainContainer.appendChild(div);
   }
 }
@@ -15,7 +16,7 @@ var embeds = document.getElementsByClassName("jcc-newsroom-widget");
 for (let embed of embeds) {
   embedArg = embed.dataset.subject;
   originUrl = embed.dataset.origin;
-  embed.insertAdjacentHTML('afterend', '<div class="embed-list" id="jcc_embed_' + embedArg + '"></div>');
+  embed.insertAdjacentHTML('beforebegin', '<div class="embed-list" id="jcc_embed_' + embedArg + '"></div>');
 
   var s = document.createElement("script"),
     callback = "jsonpCallback_" + new Date().getTime(),
