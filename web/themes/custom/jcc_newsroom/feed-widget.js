3c0d8445787179f7ce4db251a95349ea04881099
Ivan Bueno <ivan.bueno@jud.ca.gov>
2020-11-13 11:52:49 -0800

    WSGEN-1157 allow users to hide date or description

--- web/themes/custom/jcc_newsroom/feed-widget.js
+++ web/themes/custom/jcc_newsroom/feed-widget.js
@@ -1,15 +1,21 @@
 var embedArg = "";
 var originUrl = "";
 var displayCount = 10;
+var hideDescription = "no";
+var hideDate = "no";
 
 function jsonCallback(jsonData){
   let data = jsonData.items;
   displayCount = data.length < displayCount ? data.length : displayCount;
   var mainContainer = document.getElementById('jcc_embed_' + embedArg);
+
   for (var i = 0; i < displayCount; i++) {
     var div = document.createElement("div");
     div.className = "jcc-news-element";
-    div.innerHTML = '<div class="jcc-element-date">' + data[i].date_published + '</div><div class="jcc-element-title"><a href="' + originUrl + '/node/' + data[i].id + '">' + data[i].title + '</a></div><p class="jcc-element-description">' + data[i].content_html + '</p>';
+    div.innerHTML = hideDate == "no" ? '<div class="jcc-element-date">' + data[i].date_published + '</div>' : '';
+    div.innerHTML += '<div class="jcc-element-title"><a href="' + data[i].url + '">' + data[i].title + '</a></div>';
+    div.innerHTML += hideDescription == "no" ? '<p class="jcc-element-description">' + data[i].content_html + '</p>' : '';
+
     mainContainer.appendChild(div);
   }
 }
@@ -19,6 +25,8 @@ for (let embed of embeds) {
   embedArg = embed.dataset.subject;
   originUrl = embed.dataset.origin;
   displayCount = embed.dataset.count;
+  hideDescription = embed.dataset.hideDescription || "no";
+  hideDate = embed.dataset.hideDate || "no";
   embed.insertAdjacentHTML('beforebegin', '<div class="embed-list" id="jcc_embed_' + embedArg + '"></div>');
 
   var s = document.createElement("script"),
