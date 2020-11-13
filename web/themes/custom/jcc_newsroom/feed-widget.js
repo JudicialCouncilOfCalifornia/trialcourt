28d1584a55009b335953170f9a355803ada49f6b
Ivan Bueno <ivan.bueno@jud.ca.gov>
2020-11-13 12:47:18 -0800

    WSGEN-1157 allow multiple embeds on a page

--- web/themes/custom/jcc_newsroom/feed-widget.js
+++ web/themes/custom/jcc_newsroom/feed-widget.js
@@ -1,42 +1,38 @@
-var embedArg = "";
-var originUrl = "";
-var displayCount = 10;
-var hideDescription = "no";
-var hideDate = "no";
+var embedArg = [];
+var originUrl = [];
+var displayCount = [];
+var hideDescription = [];
+var hideDate = [];
 
-function jsonCallback(jsonData){
+function jsonCallback(jsonData, index){
   let data = jsonData.items;
-  displayCount = data.length < displayCount ? data.length : displayCount;
-  var mainContainer = document.getElementById('jcc_embed_' + embedArg);
+  displayCount[index] = data.length < displayCount[index] ? data.length : displayCount[index];
+  var mainContainer = document.getElementById('jcc_embed_' + embedArg[index] + '_' + index);
 
-  for (var i = 0; i < displayCount; i++) {
+  for (var i = 0; i < displayCount[index]; i++) {
     var div = document.createElement("div");
     div.className = "jcc-news-element";
-    div.innerHTML = hideDate == "no" ? '<div class="jcc-element-date">' + data[i].date_published + '</div>' : '';
+    div.innerHTML = hideDate[index] == "no" ? '<div class="jcc-element-date">' + data[i].date_published + '</div>' : '';
     div.innerHTML += '<div class="jcc-element-title"><a href="' + data[i].url + '">' + data[i].title + '</a></div>';
-    div.innerHTML += hideDescription == "no" ? '<p class="jcc-element-description">' + data[i].content_html + '</p>' : '';
+    div.innerHTML += hideDescription[index] == "no" ? '<p class="jcc-element-description">' + data[i].content_html + '</p>' : '';
 
     mainContainer.appendChild(div);
   }
 }
 
 var embeds = document.getElementsByClassName("jcc-newsroom-widget");
-for (let embed of embeds) {
-  embedArg = embed.dataset.subject;
-  originUrl = embed.dataset.origin;
-  displayCount = embed.dataset.count;
-  hideDescription = embed.dataset.hideDescription || "no";
-  hideDate = embed.dataset.hideDate || "no";
-  embed.insertAdjacentHTML('beforebegin', '<div class="embed-list" id="jcc_embed_' + embedArg + '"></div>');
+Array.prototype.forEach.call(embeds, function (embed, i) {
+  embedArg[i] = embed.dataset.subject;
+  originUrl[i] = embed.dataset.origin;
+  displayCount[i] = embed.dataset.count || "10";
+  hideDescription[i] = embed.dataset.hideDescription || "no";
+  hideDate[i] = embed.dataset.hideDate || "no";
+  embed.insertAdjacentHTML('beforebegin', '<div class="embed-list" id="jcc_embed_' + embedArg[i] + '_' + i + '"></div>');
 
-  var s = document.createElement("script"),
+  let s = document.createElement("script"),
     callback = "jsonpCallback_" + new Date().getTime(),
-    url = originUrl + '/feed/news/' + embedArg + "?callback=" + callback;
+    url = originUrl[i] + '/feed/news/' + embedArg[i] + "?callback=" + callback + "&index=" + i;
     s.type = "application/javascript";
     s.src = url;
   document.body.appendChild(s);
-}
-
-
-
-
+});
