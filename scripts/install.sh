51759d64ce9b9b0a1f1a1e4ff8e450d68033c12d
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2021-03-30 14:38:26 -0700

    Refactor multisite management.

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,7 +18,7 @@ NODB=false
 
 if [ "$NEW" = '' ] ; then
   echo -e "\n${R}No new site code provided.${RE}
-  \n Run this command with a short project code as an argument i.e. slo
+  \n Run this command with a project code as an argument i.e. slo or madera
   \n ${Y}lando multisite slo${RE}"
   exit 1
 fi
@@ -28,37 +28,14 @@ if [ -d /app/web/sites/${NEW} ] ; then
   exit 1
 fi
 
-if grep -Fq "${NEW}.lndo.site" /app/.lando.yml
-then
-  echo -e "\nProxy Detected in .lando.yml"
-else
-  echo -e "\n - ${R}No proxy ${RE}${NEW}.lndo.site${R} detected in .lando.yml.${RE}"
-  NOPROXY=true
-fi
-
-if grep -Fq "db${NEW}" /app/.lando.yml
-then
-  echo -e "\nDatabase Services for db${NEW} detected in .lando.yml"
-else
-  echo -e "\n - ${R}No Database Services for ${RE}db${NEW}${R} detected in .lando.yml${RE}"
-  NODB=true
-fi
-
-if $NODB || $NOPROXY
-then
-  echo -e "\nMake sure you update the .lando.yml file and run ${Y}lando rebuild${RE}.\n"
-  exit 1
-fi
-
 echo -e "\n${G}Creating new multisite: ${NEW}${RE}\n"
 
-echo "\$sites['${NEW}.lndo.site'] = '${NEW}';" >> /app/web/sites/sites.php
 cp -r /app/web/sites/default /app/web/sites/$NEW
 rm -rf /app/web/sites/${NEW}/files
 mkdir /app/config/config-${NEW}-local
 
 # Replace variables in settings.local.php
-sed -i "s/'host' => 'database'/'host' => 'db${NEW}'/g" /app/web/sites/${NEW}/settings.local.php
+sed -i "s/'database' => 'default'/'database' => '${NEW}'/g" /app/web/sites/${NEW}/settings.local.php
 sed -i "s/sites\/default\/services\.local\.yml/sites\/${NEW}\/services\.local\.yml/g" /app/web/sites/${NEW}/settings.local.php
 
 # Replace variables in settings.pantheon.php
@@ -73,17 +50,23 @@ sed -i "s/'..\/config\/config-default-local'/'..\/config\/config-${NEW}-local'/g
 
 sed -i "s/sites\/default\/files/sites\/default\/files\/${NEW}/g" /app/web/sites/${NEW}/settings.php
 
+# For solr config.
 sed -i "s/jcc-prod/jcc-${NEW}-live/g" /app/web/sites/${NEW}/settings.php
 sed -i "s/jcc-stage/jcc-${NEW}-stage/g" /app/web/sites/${NEW}/settings.php
 sed -i "s/jcc-dev/jcc-${NEW}-develop/g" /app/web/sites/${NEW}/settings.php
 sed -i "s/sandbox-1/jcc-${NEW}-sandbox-1/g" /app/web/sites/${NEW}/settings.php
 
+# Install site.
 echo -e "\n${G}Multisite configured... now running installation. This will take a while...${RE}"
 
 cd /app
-drush si -l ${NEW}.lndo.site -vvv --site-name="SITE NAME" --account-mail="jcc@example.com" --account-name="JCC" --account-mail="jcc@example.com"
+drush si -l ${NEW}.lndo.site -vvv -y --site-name="SITE NAME" --account-mail="jcc@example.com" --account-name="JCC" --account-mail="jcc@example.com"
 
-echo -e "\nExporting config..."
+echo -e "\nExporting config and enabling features..."
+# Enable the main feature and ensure it's imported.
+drush en jcc_tc2_all_immutable_config -l ${NEW}.lndo.site
+drush fra -y -l ${NEW}.lndo.site --bundle=jcc_tc2
+# Export to capture all initial config.
 drush cex -y -l ${NEW}.lndo.site
 
 if [ -f /app/config/config-${NEW}/config_split.config_split.local.yml ] ; then
