fdb623db10a330b30372c599b75ef9cf0cc116fa
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2021-04-27 16:58:49 -0700

    Remove old inactive multisites.

--- scripts/.fleet/db-pull.sh
+++ scripts/.fleet/db-pull.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
+show_help() {
+  name="db-pull"
+  description="Syncs source database to its local counterpart."
+  usage="scripts/fleet db-pull [env]"
+
+  echo -e "\n${G}${name}${RE}\t\t${Y}Usage:${RE}\t${usage}"
+  echo -e "\n\t\t${description}"
+}
+
+do_command() {
+  for site in $sites
+    do
+      source="@${site}.${1}"
+      local="@local.${site}"
+      echo -e "\n${B}Syncing database from ${source} to ${local}"
+      lando drush sql:sync $source $local -y &
+    done
+}
+
 case $1 in
 
   --help|-h|-?)
-    echo -e "\n${B}Syncs source database to its local counterpart.${RE}\n(Assumes pantheon site names jcc-[site], i.e. jcc-colusa)\nUsage:\n\tscripts/fleet db-pull [env]"
-    exit
+    show_help
     ;;
   *)
     env=$1
+    do_command
     ;;
 
 esac
-
-# Use drush to sync source database to local for all sites.
-for site in $sites
-do
-  source="@${site}.${1}"
-  local="@local.${site}"
-  echo -e "\n${B}Syncing database from ${source} to ${local}"
-  lando drush sql:sync $source $local -y &
-done
---
+++ scripts/.fleet/drush.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+show_help() {
+  name="drush"
+  description="Runs a drush command on all sites, in the given environment."
+  usage="scripts/fleet drush [env] [command] [options]"
+
+  echo -e "\n${G}${name}${RE}\t\t${Y}Usage:${RE}\t${usage}"
+  echo -e "\n\t\t${description}"
+}
+
+do_command() {
+  for site in $sites
+  do
+    [[ "$env" == "local" ]] && alias="@local.${site}" || alias="@${site}.${env}"
+
+    echo -e "\n${B}Running ${RE}drush ${alias} ${command} ${@}"
+    lando drush $alias $command $@
+  done
+}
+
+case $1 in
+
+  --help|-h|-?)
+    show_help
+    ;;
+  *)
+    env=$1
+    command=$2
+    shift 2
+
+    if [[ -z "$env" ]] || [[ -z "$command" ]] ; then
+      echo -e "\n${R}You must enter a valid [environment] and [command].${RE}"
+      show_help
+    fi
+    do_command $@
+    ;;
+
+esac
