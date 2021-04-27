c3120db4e9ebf7cdefde290505382262cdfeebca
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2021-04-27 16:58:49 -0700

    Remove old inactive multisites.

--- .circleci/scripts/project-default.sh
+++
@@ -1,5 +0,0 @@
-# Pantheon Project UUID.
-UUID=9c57991e-d34c-46f8-9ced-99a0c9fe9959
-SITE_CODE=default
-# If false, pantheon will deploy to dev. If true it will get the Live tag.
-LIVE=true
--- scripts/.fleet/backup.sh
+++ scripts/.fleet/backup.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname $(dirname $(realpath $0)))
+show_help() {
+  name="backup"
+  description="Runs a pantheon backup for all sites in the sites directory."
+  usage="scripts/fleet backup [env]"
+
+  echo -e "\n${G}${name}${RE}\t\t${Y}Usage:${RE}\t${usage}"
+  echo -e "\n\t\t${description}"
+}
+
+do_command() {
+  for site in $sites
+    do
+      site="jcc-${site}.${env}"
+      echo -e "\n${B}Creating backup for ${site}${RE}"
+      terminus backup:create ${site} &
+    done
+}
 
 case $1 in
 
   --help|-h|-?)
-    echo -e "\n${B}This script runs a pantheon backup for all sites in the sites directory. Just pass in the environment.${RE}\n(Assumes pantheon site names jcc-[site], i.e. jcc-colusa)\n\nUsage:\n\tscripts/fleet backup [env]"
-    exit
+    show_help
     ;;
   *)
     env=$1
+    do_command
     ;;
 
 esac
-
-# A simple script to create a backup of all pantheon sites for a given env.
-# Requires terminus and authentication.
-# Usage:  scripts/backup.sh [develop | stage | live | epic-*]
-
-for site in $sites
-do
-  site="jcc-${site}.${env}"
-  echo -e "\n${B}Creating backup for ${site}${RE}"
-  terminus backup:create ${site} &
-done
