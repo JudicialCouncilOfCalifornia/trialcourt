4fd1b39ee1f8d50102ba13c2927a94b6cede378c
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2021-05-12 10:15:55 -0700

    Retool for sierra and tulare because of a pantheon issue.

--- .circleci/scripts/project-sierra.sh
+++ .circleci/scripts/project-sierra.sh
@@ -1,5 +1,5 @@
 # Pantheon Project UUID.
-UUID=995e5203-2f6a-43a3-bac8-4191d8b870e8
+UUID=61b3dcd4-8de6-427a-ac35-c0d5320eb066
 SITE_CODE=sierra
 # If false, pantheon will deploy to dev. If true it will get the Live tag.
 LIVE=true
