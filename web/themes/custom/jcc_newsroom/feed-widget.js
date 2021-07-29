21d57da458802a7d57995bb01912190413a482cd
Mel Wong <mel.wong@jud.ca.gov>
2021-07-28 23:10:52 -0700

    SC-42: originUrl was not reliable so back to manually constructing image domain

--- web/themes/custom/jcc_newsroom/feed-widget.js
+++ web/themes/custom/jcc_newsroom/feed-widget.js
@@ -31,9 +31,19 @@ function jsonCallback(jsonData, index){
     }
 
     // Card image ... extract Drupal image element in JSON result (e.g. media library thumbnail)
+    var hostname = window.location.hostname;
     var image = decodeURIComponent(data[i].image);
-    //var imageDomain = 'https://newsroom.courts.ca.gov';
-    var imageUrl = originUrl[i] + image.match(/src="([^"]*)/)[1];
+    var imageDomain = "https://newsroom.courts.ca.gov";
+    if (hostname.includes("lndo.site")) {
+      imageDomain = "http://newsroom.lndo.site";
+    } else if (hostname.includes("pantheonsite.io")) {
+      if (hostname.includes("stage-")) {
+        imageDomain = "https://stage-jcc-newsroom.pantheonsite.io";
+      } else {
+        imageDomain = "https://develop-jcc-newsroom.pantheonsite.io";
+      }
+    }
+    var imageUrl = imageDomain + image.match(/src="([^"]*)/)[1];
     var imageAlt = image.match(/alt="([^"]*)/)[1];
     var divImage = document.createElement("div");
     divImage.className = "jcc-element-image usa-card__media";
