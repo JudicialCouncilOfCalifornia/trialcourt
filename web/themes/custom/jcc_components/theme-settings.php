fd53935c480df44bbbcd8f8a4c76c8ee0bdd62a4
Rob Decker <rob@deckerdev.com>
2021-08-17 14:15:00 -0700

    TCI-562 Add Google Translate widget option

--- web/themes/custom/jcc_components/theme-settings.php
+++ web/themes/custom/jcc_components/theme-settings.php
@@ -151,4 +151,11 @@ function jcc_components_form_system_theme_settings_alter(&$form, FormStateInterf
     '#default_value' => theme_get_setting('hide_translation'),
     '#description'   => t("Hide translation dropdown from header."),
   ];
+
+  $form['global']['show_google_translate'] = [
+    '#type'          => 'checkbox',
+    '#title'         => t('Show Google translator'),
+    '#default_value' => theme_get_setting('show_google_translate'),
+    '#description'   => t("Show Google translation dropdown in header."),
+  ];
 }
