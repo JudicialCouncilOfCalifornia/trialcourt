7952b19788acaa89d4a5eee290dbbd98fcd36f87
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2021-10-25 20:58:20 -0700

    TCI-663: Spin up San Benito

---
+++ .circleci/scripts/project-sanbenito.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=9fd5af67-8914-41f1-bd7b-b85d19be3a53
+SITE_CODE=sanbenito
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
