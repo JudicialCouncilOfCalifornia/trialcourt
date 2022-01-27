b7d8f572a346c361ef9afd340d58e5f96727b3e7
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-01-27 15:17:02 -0800

    Scripts to get frequent visitors, visited urls, and user agents

---
+++ scripts/getstats.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+name=$1
+BASEDIR=$(dirname $(dirname $(realpath $0)))
+
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+RE="\033[0m"
+
+if [ -f data/logs/${name}/aggregate-logs/combined.logs ] ; then
+  echo -e "\n${Y}Log data/logs/${name}/aggregate-logs/combined.logs already exists.${RE}\n"
+
+  read -p "Replace? (y/N)" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    . ${BASEDIR}/scripts/getlogs.sh ${name}
+  fi
+else
+  . ${BASEDIR}/scripts/getlogs.sh ${name}
+fi
+
+if [ -z "$2" ]
+  then
+    echo -e "FREQUENT VISITOR IPs"
+    cat data/logs/${name}/aggregate-logs/combined.logs | awk -F '\"' '{ print $8 }' | awk -F ',' '{print $1}' | sort | uniq -c | sort -frn | head -n 25
+    echo -e "\nFREQUENT VISITED URLs"
+    cat data/logs/${name}/aggregate-logs/combined.logs | awk -F '\"' '{print $2}' | sort | uniq -c | sort -nr | head
+    echo -e "\nFREQUENT USER AGENTS"
+    cat data/logs/${name}/aggregate-logs/combined.logs | awk -F '\"' '{print $6}' | sort | uniq -c | sort -nr | head
+else
+    echo -e "FREQUENT VISITOR IPs"
+    tail -n $2 data/logs/${name}/aggregate-logs/combined.logs | awk -F '\"' '{ print $8 }' | awk -F ',' '{print $1}' | sort | uniq -c | sort -frn | head -n 25
+    echo -e "\nFREQUENT VISITED URLs"
+    tail -n $2  data/logs/${name}/aggregate-logs/combined.logs | awk -F '\"' '{print $2}' | sort | uniq -c | sort -nr | head
+    echo -e "\nFREQUENT USER AGENTS"
+    tail -n $2  data/logs/${name}/aggregate-logs/combined.logs | awk -F '\"' '{print $6}' | sort | uniq -c | sort -nr | head
+fi
