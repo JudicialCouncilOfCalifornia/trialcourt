f9ff4a1637a8bd8b1e8d9df13ae8f61f550925f3
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2022-03-10 17:28:13 -0800

    TCI-844: Spin Up Kern.  Fix installation dependency issue.

---
+++ .circleci/scripts/project-kern.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=c3531edd-1cbc-4c32-9b59-e915af6dd162
+SITE_CODE=kern
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
