16fa8717e670ecda9df4b78da7001d80e518635f
Mel Wong <mel.wong@jud.ca.gov>
2022-04-05 15:51:34 -0700

    SC-337: improvements to case string detection & display events

--- web/modules/custom/jcc_case_lookup/js/jcc-case-lookup.js
+++ web/modules/custom/jcc_case_lookup/js/jcc-case-lookup.js
@@ -12,27 +12,33 @@
    */
   Drupal.behaviors.jccCaseLookup = {
     attach: function (context) {
-      var docketSearch = '<div id="docket_search_bynumber" style="position:absolute; display:block; width:100%; background-color:#fff; top:2.5rem">' +
-        '<form class="sr-only" name="searchFormNumber" id="searchFormNumber" method="post" action="https://appellatecases.courtinfo.ca.gov/search/searchResults.cfm?dist=0&search=number">' +
-        '<input type="hidden" name="query_caseNumber" id="query_caseNumber" />' +
-        '<input type="hidden" name="bot_check_1" id="bot_check_1" value="Y" checked> <span id="bot_text_1">I am a robot</span>' +
-        '<input type="hidden" name="bot_check_6" id="bot_check_6" value="Y">' +
-        //'<input class="btn btn-primary" type="submit" name="submit" id="caseNumberSubmit" value="Search by Case Number"/></p>' +
-        '</form>' +
-        '<a id="lookupDocket" href="#" style="color: #000;">Look up docket at ACCMS from this case number?</a>'
-      '</div>';
-
       $(document).ready(function() {
-        if ($('#docket_search_bynumber').length == 0) {
-          $('.usa-search').append(docketSearch);
-        }
-
         $('.usa-search input').on('keyup', function (e) {
-          var number = $('input[type="search"]').val();
-          $('#query_caseNumber').val(number);
+          var query = $('input[type="search"]').val();
+          var caseNumber = query.match(/[Ss][0-9]{6}/g);
+
+          // Display docket search option only if case number entered (e.g. S######).
+          if (caseNumber) {
+            if ($('#docket-search-bynumber').length == 0) {
+              // Docket search contextual modal.
+              var docketSearch = '<div id="docket-search-bynumber" class="docket-search-bynumber">' +
+                '<form class="sr-only" name="searchFormNumber" id="searchFormNumber" method="post" target="_blank" action="https://appellatecases.courtinfo.ca.gov/search/searchResults.cfm?dist=0&search=number">' +
+                '<input type="text" name="query_caseNumber" id="query_caseNumber" />' +
+                '<input type="checkbox" name="bot_check_1" id="bot_check_1" value="Y" checked> <span id="bot_text_1">I am a robot</span>' +
+                '<input type="checkbox" name="bot_check_6" id="bot_check_6" value="Y">' +
+                '</form>' +
+                'Looking for docket information? <a id="lookupDocket" href="#" aria-label="Look up case ' + caseNumber + ' at the Appellate Courts Case Information site in a new browser window.">Look up case ' + caseNumber + '</a> at the Appellate Courts Case Information site.' +
+              '</div>';
+              $('.usa-search').append(docketSearch);
+            }
+            // Set
+            $('#query_caseNumber').val(caseNumber);
+          } else {
+            $('#docket-search-bynumber').remove();
+          }
         });
 
-        $(document).on('click', '#lookupDocket', function() {
+        $(document).on('click', '#lookupDocket', function () {
           $('form[name="searchFormNumber"]').submit();
         });
       });
