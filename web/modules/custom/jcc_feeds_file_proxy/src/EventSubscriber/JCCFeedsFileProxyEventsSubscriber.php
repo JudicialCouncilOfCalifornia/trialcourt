bf385981753751f75696842905b4802cbdeafef7
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2022-07-06 18:07:21 -0700

    Epic-d9 use dependency injection for Baptiste's fix. Clean up commented out code.

--- web/modules/custom/jcc_feeds_file_proxy/src/EventSubscriber/JCCFeedsFileProxyEventsSubscriber.php
+++ web/modules/custom/jcc_feeds_file_proxy/src/EventSubscriber/JCCFeedsFileProxyEventsSubscriber.php
@@ -26,8 +26,9 @@ class JCCFeedsFileProxyEventsSubscriber implements EventSubscriberInterface {
   /**
    * Constructor.
    */
-  public function __construct(RequestStack $request_stack) {
+  public function __construct(RequestStack $request_stack, FileSystemInterface $file_repository) {
     $this->request = $request_stack->getCurrentRequest();
+    $this->fileRepository = $file_repository;
   }
 
   /**
@@ -35,7 +36,8 @@ class JCCFeedsFileProxyEventsSubscriber implements EventSubscriberInterface {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('request_stack')
+      $container->get('request_stack'),
+      $container->get('file.repository')
     );
   }
 
@@ -95,12 +97,7 @@ class JCCFeedsFileProxyEventsSubscriber implements EventSubscriberInterface {
 
     $filename = "public://newslink/" . $img_id . ".png";
     if (!file_exists($filename)) {
-//      $file = file_save_data($file_data, $filename, FILE_EXISTS_REPLACE);
-
-//      $filesaved = file_save_data($file, $destination, FileSystemInterface::EXISTS_REPLACE);
-      $fileRepository = \Drupal::service('file.repository');
-      $file = $fileRepository->writeData($file_data, $filename, FileSystemInterface::EXISTS_REPLACE);
-
+      $file = $this->fileRepository->writeData($file_data, $filename, FileSystemInterface::EXISTS_REPLACE);
 
       $media = Media::create([
         'bundle' => 'image',
