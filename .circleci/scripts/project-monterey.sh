f6a6be500bc9863c65266142d169746408ccc7a7
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-07-19 15:54:23 -0700

    create monterey site

---
+++ .circleci/scripts/project-monterey.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=fe7e63af-e215-43ae-b760-5a0b683f1d2b
+SITE_CODE=monterey
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
