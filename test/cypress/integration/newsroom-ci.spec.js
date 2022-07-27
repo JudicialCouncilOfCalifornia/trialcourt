937e76b50262cfdda03b8219705ef29c06319dae
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-07-27 14:13:54 -0700

    Reuse common tests across specs

--- test/cypress/integration/newsroom-ci.spec.js
+++ test/cypress/integration/newsroom-ci.spec.js
@@ -1,4 +1,6 @@
-const common_pages = [
+import { commonTests } from "../support/common";
+
+const main_pages = [
   '/',
   '/news',
   '/calendar',
@@ -7,36 +9,5 @@ const common_pages = [
 ];
 
 describe('Newsroom Smoke Test', () => {
-
-  describe('Check main pages', () => {
-    common_pages.forEach((page) => {
-      it(`visits ${page} and checks for copyright`, () => {
-        cy.visit(page)
-        cy.contains('© 2022');
-      })
-    })
-  });
-
-  describe('Validate search', () => {
-    it("searches for 'pretrial'", () => {
-      cy.visit('/');
-      cy.get('input[name="keywords"]').type('pretrial{enter}', { force: true });
-      cy.contains('No results found.').should('not.exist');
-    })
-  });
-
-  describe('User Login and Administration', () => {
-    it("logs in user", () => {
-      cy.visit('/user/login');
-      cy.get('input[name="name"]').type(Cypress.env('username'));
-      cy.get('input[name="pass"]').type(Cypress.env('password') + '{enter}');
-      cy.contains('Member for').should('exist');
-    })
-
-    it("logs out user", () => {
-      cy.visit('/user/logout');
-      cy.contains('Bueno, Ivan').should('not.exist');
-    })
-  });
-
+  commonTests(main_pages, 'keywords', 'No results found.');
 })
