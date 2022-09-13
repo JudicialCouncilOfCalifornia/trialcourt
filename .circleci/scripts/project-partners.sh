c9056d453b97b6441a3cd2dae415511e076542f8
Mel Wong <mel.wong@jud.ca.gov>
2022-09-12 19:31:57 -0700

    Add partners site

---
+++ .circleci/scripts/project-partners.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=193d81ea-5227-4c9b-842b-24b3dbeb5ca5
+SITE_CODE=partners
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
