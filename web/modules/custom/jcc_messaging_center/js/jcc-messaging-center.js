2eea66cd9e6b501cd7433d0c3b8f2f38ec9c09b6
Baptiste Niviere <baptiste.niviere@gmail.com>
2022-10-03 18:40:22 -0700

    dynamic preselection

--- web/modules/custom/jcc_messaging_center/js/jcc-messaging-center.js
+++ web/modules/custom/jcc_messaging_center/js/jcc-messaging-center.js
@@ -13,16 +13,17 @@
   Drupal.behaviors.jccMessagingCenter = {
     attach: function (context, settings) {
       // enabling hidden field for subscriptions
-      $('.jcc-messaging-center-section-form', context).each(function(index, element){
-        $('.jcc-messaging-center-section-form .form-checkbox').prop('checked', false );
-        var group_val = $(this).attr('data-group-values');
-        var group_val_array = group_val.split("-");
-        $('.jcc-messaging-center-section-form .fieldgroup .form-checkbox', context).each(function(index, element) {
-          if(group_val_array.includes($(this).attr('value'))){
-            $(this).prop('checked', true );
+      if($('.user-register-form', context).length){
+        var urlParams = new URLSearchParams(window.location.search);
+        var stringargs = urlParams.getAll('preselected');
+        if(stringargs.length){
+          $('.user-register-form .field--name-field-group .form-checkboxes input', context).prop('checked', false);
+          var args = stringargs[0].split(',');
+          for (var i = 0; i < args.length; i++) {
+            $('.user-register-form .field--name-field-group .form-checkboxes input[value='+args[i]+']', context).prop('checked', true);
           }
-        })
-      })
+        }
+      }
     }
   }
 } (jQuery, Drupal));
