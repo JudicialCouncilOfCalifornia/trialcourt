2c6911ab253aab7ffab1842c0398912c66895805
Ivan Bueno <ivan.bueno@jud.ca.gov>
2023-03-09 12:28:56 -0800

    update script

--- .circleci/scripts/conflict-marker-check.sh
+++ .circleci/scripts/conflict-marker-check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -eo pipefail
 
 conflicted="$( grep -rE --exclude-dir={vendor,node_modules} --include=\*.{module,inc,php,js,css,html,htm,profile,install,yml,md} "^[\<]{7}" )"
 if [[ -n "$conflicted" ]] ; then exit 1 ; else exit 0 ; fi
