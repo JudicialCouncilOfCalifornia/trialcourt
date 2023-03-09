8c92c539ca3493ae6d7f137fb013628fbc91dc98
Ivan Bueno <ivan.bueno@jud.ca.gov>
2023-03-09 11:42:55 -0800

    conflict checker script

---
+++ .circleci/scripts/conflict-marker-check.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+grep -qlrE --exclude-dir={vendor,node_modules} --include=\*.{module,inc,php,js,css,html,htm,profile,install,yml,md} "^[\<]{7}"
+if [ $? -eq 0 ] ; then exit 1 ; else exit 0 ; fi
