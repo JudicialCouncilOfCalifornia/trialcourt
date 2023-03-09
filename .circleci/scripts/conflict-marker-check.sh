7228ab9811cd8b05a47f373cf256d49ea508c07c
Ivan Bueno <ivan.bueno@jud.ca.gov>
2023-03-09 12:02:56 -0800

    update script

--- .circleci/scripts/conflict-marker-check.sh
+++ .circleci/scripts/conflict-marker-check.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
-
 set -e
 
-# Exit script if you try to use an uninitialized variable.
-set -o nounset
-
-# Exit script if a statement returns a non-true return value.
-set -o errexit
-
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
-
-conflicted="$( grep -qlrE --exclude-dir={vendor,node_modules} --include=\*.{module,inc,php,js,css,html,htm,profile,install,yml,md} "^[\<]{7}" )"
-if [[ -n "$conflicted" ]] ; then echo found; fi
+conflicted="$( grep -lrE --exclude-dir={vendor,node_modules} --include=\*.{module,inc,php,js,css,html,htm,profile,install,yml,md} "^[\<]{7}" )"
+if [[ -n "$conflicted" ]] ; then exit 1; fi
 
 
