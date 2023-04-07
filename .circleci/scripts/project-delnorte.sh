967c3e96fba12a554aa4ba36ddd819a975b07d64
Archana <archana.ramalingam@jud.ca.gov>
2023-04-07 12:45:02 -0700

    create Del Norte

---
+++ .circleci/scripts/project-delnorte.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=51d370d9-5693-4487-bd0e-0c379f37e562
+SITE_CODE=delnorte
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
