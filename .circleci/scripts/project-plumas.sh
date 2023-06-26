c833afc8abe2a3fc94609ad2437a520f0f8846e2
Archana <archana.ramalingam@jud.ca.gov>
2023-06-26 12:55:36 -0700

    create Plumas

---
+++ .circleci/scripts/project-plumas.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=d3dc8801-d5fb-4142-a539-fa25424258e8
+SITE_CODE=plumas
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
