aea3d7a32cbde1f4fcdb4fc642fd7a6e6b6cd434
Mel Wong <mel.wong@jud.ca.gov>
2023-07-06 19:12:53 -0700

    RJDT-1: new site setup

---
+++ .circleci/scripts/project-diversity-toolkit.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=046846be-1ce5-4536-8f8d-e0e32a89fe1e
+SITE_CODE=diversity-toolkit
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
