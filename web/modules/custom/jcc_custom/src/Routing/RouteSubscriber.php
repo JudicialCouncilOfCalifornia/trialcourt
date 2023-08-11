51c7191d4ee5f3aab0bad0022b44a69dfc4b115d
Mel Wong <mel.wong@jud.ca.gov>
2023-08-11 11:36:51 -0700

    DCAS-69: granular menu management & settings access

--- web/modules/custom/jcc_custom/src/Routing/RouteSubscriber.php
+++ web/modules/custom/jcc_custom/src/Routing/RouteSubscriber.php
@@ -3,6 +3,7 @@
 namespace Drupal\jcc_custom\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
@@ -10,13 +11,43 @@ use Symfony\Component\Routing\RouteCollection;
  */
 class RouteSubscriber extends RouteSubscriberBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -1025];
+    return $events;
+  }
 
   /**
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    if ($route = $collection->get('protected_pages_list')) {
-      $route->setDefault('_controller', '\Drupal\jcc_custom\Controller\ProtectedPagesController::protectedPagesList');
+    $routes = $collection->all();
+    foreach ($routes as $route_name => $route) {
+      switch ($route_name) {
+        case 'protected_pages_list':
+          $route->setDefault('_controller', '\Drupal\jcc_custom\Controller\ProtectedPagesController::protectedPagesList');
+          break;
+
+        case 'entity.menu.collection':
+        case 'entity.menu.edit_form':
+        case 'entity.menu.add_link_form':
+        case 'menu_ui.link_edit':
+        case 'menu_ui.link_reset':
+        case 'entity.menu_link_content.canonical':
+        case 'entity.menu_link_content.delete_form':
+        case 'entity.menu_link_content.content_translation_overview':
+        case 'entity.menu_link_content.content_translation_add':
+        case 'entity.menu_link_content.content_translation_edit':
+        case 'entity.menu_link_content.content_translation_delete':
+          // Restrict menu management toolbar for elevated sites.
+          // Translators edit requests under /admin/tmgmt/jobs.
+          if (jcc_custom_is_elevated_site()) {
+            $route->setRequirement('_role', 'administrator');
+          }
+          break;
+      }
     }
   }
 
