3887d417de1ab50f2b134abece5c746f5d0ce13b
Archana <archana.ramalingam@jud.ca.gov>
2023-09-26 15:20:54 -0700

    test site

---
+++ .circleci/scripts/project-phptest.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=bc896365-d487-468f-ac66-eb9b0d5a3c41
+SITE_CODE=phptest
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
