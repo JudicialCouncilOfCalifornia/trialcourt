31a850e7f0d7c0d87c21c88ecce6e90479db0c31
Archana <archana.ramalingam@jud.ca.gov>
2023-09-28 19:58:03 -0700

    create Marin

---
+++ .circleci/scripts/project-marin.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=1f06a41c-2e41-4227-bc8c-8c77458292ae
+SITE_CODE=marin
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
