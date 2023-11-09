8d592d5508c966d670921a2e47d0e2659570f6da
Archana <archana.ramalingam@jud.ca.gov>
2023-11-09 10:25:08 -0800

    Changes for PHP8 deployment

--- .circleci/scripts/project-delnorte.sh
+++ .circleci/scripts/project-delnorte.sh
@@ -2,6 +2,6 @@
 UUID=854a7e04-0ced-47ce-82ae-94dd954f79ca
 SITE_CODE=delnorte
 # If false, pantheon will deploy to dev. If true it will get the Live tag.
-LIVE=true
+LIVE=false
 # If false, deployments will be skipped for the master branch for this site.
 DEPLOY_MASTER=true
--- .circleci/scripts/project-santabarbara.sh
+++ .circleci/scripts/project-santabarbara.sh
@@ -2,6 +2,6 @@
 UUID=74785d8c-e8f2-42ad-a70e-e0f071b03efa
 SITE_CODE=santabarbara
 # If false, pantheon will deploy to dev. If true it will get the Live tag.
-LIVE=true
+LIVE=false
 # If false, deployments will be skipped for the master branch for this site.
 DEPLOY_MASTER=true
