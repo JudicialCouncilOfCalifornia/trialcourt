c116b4b9e7ccf5da34b703e69d527f404d35b899
Philip Boden <phil@chapterthreellc.com>
2023-11-30 00:10:09 -0800

    [DCAS-177] -- Appellate search by Zip/city/county form and map block for QA

--- web/modules/custom/jcc_elevated_embeds/src/Controller/JccElevatedEmbedZipCityCountyAutoCompleteController.php
+++ web/modules/custom/jcc_elevated_embeds/src/Controller/JccElevatedEmbedZipCityCountyAutoCompleteController.php
@@ -2,17 +2,17 @@
 
 namespace Drupal\jcc_elevated_embeds\Controller;
 
-use ArrayQuery\QueryBuilder;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Routing\AdminContext;
+use Drupal\jcc_elevated_sections\JccSectionServiceInterface;
+use Drupal\path_alias\AliasManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\Component\Utility\Xss;
-use Drupal\Core\Entity\Element\EntityAutocomplete;
 
 /**
  * Defines a route controller for watches autocomplete form elements.
@@ -55,6 +55,20 @@ class JccElevatedEmbedZipCityCountyAutoCompleteController extends ControllerBase
   protected $moduleHandler;
 
   /**
+   * The path alias handler service.
+   *
+   * @var \Drupal\path_alias\AliasManagerInterface
+   */
+  protected $aliasManager;
+
+  /**
+   * The jcc section service.
+   *
+   * @var \Drupal\jcc_elevated_sections\JccSectionService
+   */
+  protected $jccSectionService;
+
+  /**
    * Constructs media integration plugin.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
@@ -65,12 +79,26 @@ class JccElevatedEmbedZipCityCountyAutoCompleteController extends ControllerBase
    *   Cache service.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   Module handler.
+   * @param \Drupal\path_alias\AliasManagerInterface $alias_manager
+   *   Path alias manager handler.
+   * @param \Drupal\jcc_elevated_sections\JccSectionServiceInterface $jcc_section_service
+   *   JCC Section service.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, AdminContext $admin_context, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager,
+                              AdminContext $admin_context,
+                              CacheBackendInterface $cache_backend,
+                              ModuleHandlerInterface $module_handler,
+                              AliasManagerInterface $alias_manager,
+                              JccSectionServiceInterface $jcc_section_service) {
     $this->entityTypeManager = $entity_type_manager;
     $this->adminContext = $admin_context;
     $this->cache = $cache_backend;
     $this->moduleHandler = $module_handler;
+    $this->aliasManager = $alias_manager;
+    $this->jccSectionService = $jcc_section_service;
     $this->nodeStorage = $entity_type_manager->getStorage('node');
   }
 
@@ -83,6 +111,8 @@ class JccElevatedEmbedZipCityCountyAutoCompleteController extends ControllerBase
       $container->get('router.admin_context'),
       $container->get('cache.default'),
       $container->get('module_handler'),
+      $container->get('path_alias.manager'),
+      $container->get('jcc_elevated_sections.service'),
     );
   }
 
@@ -95,20 +125,42 @@ class JccElevatedEmbedZipCityCountyAutoCompleteController extends ControllerBase
     $input = $request->query->get('q');
 
     // Get the typed string from the URL, if it exists.
-    if (!$input || strlen($input) <= 2 ) {
-      return new JsonResponse([t('Please enter more characters for your search.')]);
+    if (!$input || strlen($input) <= 2) {
+      return new JsonResponse([$this->t('Please enter more characters for your search.')]);
     }
 
-    $input = Xss::filter($input);
-    $items = $this->getCsvData();
+    // Grab our input, and break it up if a multiple terms with the full search
+    // as the first searched item. So "Valley springs" would be searched as
+    // "Valley springs", "Valley", and "springs".
+    $input = trim(strtolower(Xss::filter($input)));
+    $input_terms = explode(' ', $input);
+    if (count($input_terms) > 1) {
+      array_unshift($input_terms, $input);
+    }
 
-    $results = array_filter($items, function($location_data) use ($input){
-      return strpos($location_data, $input) !== FALSE;
-    }, ARRAY_FILTER_USE_KEY);
+    // Remove CA and County from searching terms (they are on every item).
+    $cleaned_input_terms = array_filter($input_terms, function ($term) {
+      // We don't use ca, county, or san as extra search terms because they will
+      // return too much data.
+      return ($term !== 'ca') && ($term !== 'county') && ($term !== 'san') ? $term : FALSE;
+    });
+
+    $items = $this->getData();
+
+    // Go through each individual term and collect results.
+    foreach ($cleaned_input_terms as $input_term) {
+      $results += array_filter($items, function ($location_data) use ($input_term) {
+        $input_term = preg_replace('/[\W]/', '', $input_term);
+        return strpos(strtolower($location_data), $input_term) !== FALSE;
+      }, ARRAY_FILTER_USE_KEY);
+    }
 
     if (empty($results)) {
-      return new JsonResponse([t('No results found. Please try another search.')]);
+      return new JsonResponse([$this->t('No results found. Please try another search.')]);
     }
+
+    // Return only the first 10 items.
+    $results = array_slice($results, 0, 40);
 
     return new JsonResponse($results);
   }
@@ -118,43 +170,71 @@ class JccElevatedEmbedZipCityCountyAutoCompleteController extends ControllerBase
    */
   protected function getCsvData() {
     $data = [];
+
     $modulePath = $this->moduleHandler->getModule('jcc_elevated_embeds')->getPath();
     $csv = $modulePath . "/assets/appellate_zip_city_county_search/JccDistrictsByZipCityCounty__ProcessedInformation.csv";
+
     $file = fopen($csv, 'r');
     while (!feof($file)) {
       $csv_file = fgetcsv($file, '', ",");
       $data[$csv_file[0]] = $csv_file[1];
     }
     fclose($file);
+
     unset($data['Location_data']);
+
     return $data;
   }
 
+  /**
+   * Handler for autocomplete request.
+   */
+  protected function getData() {
+    $cacheId = 'jcc_elevated_embeds:appellate_zcs_processed_source_data';
 
+    if ($cache = $this->cache->get($cacheId)) {
+      return $cache->data;
+    }
 
+    $data = $this->getCsvData();
 
+    $section_service = $this->jccSectionService;
+    $sections = $section_service->getSections();
 
-//    $input = Xss::filter($input);
-//    $items = $this->getCsvData();
-//
-//    $qb = QueryBuilder::create($items);
-//    $qb->sortedBy('city', 'DESC')
-//      ->limit(0, 10);
-//
-//    $qb->addCriterion(0, $input, 'CONTAINS');
-//    if (is_numeric($input)) {
-//      $qb->addCriterion('zip', $input, 'CONTAINS');
-//    }
-//    else {
-//      $qb->addCriterion('city', $input, 'CONTAINS');
-//    }
-//
-//    $qresults = $qb->getResults();
-//
-//    if (empty($qresults)) {
-//      return new JsonResponse([t('No results found. Please try another search.')]);
-//    }
-//    foreach ($qresults as $item) {
-//      $results[] = $item['full'];
-//    }
+    $urls = [];
+    foreach ($sections as $section) {
+      $machine_name = $section->get('jcc_section_machine_name')->value;
+      $sh_nid = $section->get('jcc_section_homepage')->target_id;
+      $alias = $this->aliasManager->getAliasByPath('/node/' . $sh_nid);
+      $urls[$machine_name] = [
+        'machine_name' => $machine_name,
+        'name' => $section->label(),
+        'sid' => $section->id(),
+        'url' => $alias,
+      ];
+    }
+
+    // District ID from data should match machine name from above.
+    $revised_data = [];
+    foreach ($data as $zip_city_info => $district_id) {
+      $info = explode(' | ', $zip_city_info);
+      $zip = $info[0];
+      $city = str_replace(", CA", "", $info[1]);
+      $county = str_replace(" County", "", $info[2]);
+      $revised_data[$zip . " | " . $county . " | " . $city] = [
+        'district' => $urls[$district_id]['name'],
+        'machine_name' => $urls[$district_id]['machine_name'],
+        'url' => $urls[$district_id]['url'],
+        'search' => $zip . " | " . $city . " | " . $county,
+        'zip' => $info[0],
+        'city' => $city,
+        'county' => $county,
+      ];
+    }
+
+    $this->cache->set($cacheId, $revised_data, CacheBackendInterface::CACHE_PERMANENT);
+
+    return $revised_data;
+  }
+
 }
--- web/modules/custom/jcc_elevated_embeds/src/Plugin/Block/JccAppellateZipCitySearchBlock.php
+++ web/modules/custom/jcc_elevated_embeds/src/Plugin/Block/JccAppellateZipCitySearchBlock.php
@@ -2,21 +2,23 @@
 
 namespace Drupal\jcc_elevated_embeds\Plugin\Block;
 
-use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\AdminContext;
+use Drupal\Core\Url;
+use Drupal\jcc_elevated_sections\JccSectionServiceInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'JccAppellateZipCitySearch' block.
  *
  * @Block(
- *  id = "jcc_appellate_zip_city_search",
+ *  id = "jcc_appellate_zcs",
  *  admin_label = @Translation("JCC Appellate Zip/City Search Block"),
  * )
  */
@@ -51,6 +53,13 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
   protected $moduleHandler;
 
   /**
+   * The jcc section service.
+   *
+   * @var \Drupal\jcc_elevated_sections\JccSectionService
+   */
+  protected $jccSectionService;
+
+  /**
    * Constructs media integration plugin.
    *
    * @param array $configuration
@@ -67,14 +76,25 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
    *   Cache service.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   Module handler.
+   * @param \Drupal\jcc_elevated_sections\JccSectionServiceInterface $jcc_section_service
+   *   JCC Section service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, AdminContext $admin_context, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
+  public function __construct(array $configuration,
+                              $plugin_id,
+                              $plugin_definition,
+                              EntityTypeManagerInterface $entity_type_manager,
+                              AdminContext $admin_context,
+                              CacheBackendInterface $cache_backend,
+                              ModuleHandlerInterface $module_handler,
+                              JccSectionServiceInterface $jcc_section_service) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
     $this->entityTypeManager = $entity_type_manager;
     $this->adminContext = $admin_context;
     $this->cache = $cache_backend;
     $this->moduleHandler = $module_handler;
+    $this->jccSectionService = $jcc_section_service;
+    $this->nodeStorage = $entity_type_manager->getStorage('node');
   }
 
   /**
@@ -89,6 +109,7 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
       $container->get('router.admin_context'),
       $container->get('cache.default'),
       $container->get('module_handler'),
+      $container->get('jcc_elevated_sections.service'),
     );
   }
 
@@ -115,7 +136,77 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
     $form['#tree'] = TRUE;
+
+    $form['description'] = [
+      '#title' => $this->t('Description'),
+      '#rows' => 3,
+      '#cols' => 60,
+      '#type' => 'textarea',
+      '#default_value' => $config['description'] ?? '',
+    ];
+
+    $form['view_all'] = [
+      '#type' => 'details',
+      '#title' => $this->t('View all link'),
+    ];
+
+    $form['view_all']['view_all_title'] = [
+      '#title' => $this->t('Title'),
+      '#type' => 'textfield',
+      '#default_value' => $config['view_all_title'] ?? $this->t('See all courts'),
+    ];
+
+    $form['view_all']['view_all_url'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'node',
+      '#title' => $this->t('Url'),
+      '#description' => $this->t('Internal or external urls are allowed.'),
+      '#required' => TRUE,
+      '#process_default_value' => FALSE,
+      '#default_value' => $config['view_all_url'] ?? '',
+      '#attributes' => [
+        'data-autocomplete-first-character-blacklist' => '/#?',
+      ],
+      '#element_validate' => [
+        [
+          'Drupal\link\Plugin\Field\FieldWidget\LinkWidget',
+          'validateUriElement',
+        ],
+      ],
+    ];
+
+    $form['fmc'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Find my court'),
+    ];
+
+    $form['fmc']['fmc_title'] = [
+      '#title' => $this->t('Title'),
+      '#type' => 'textfield',
+      '#default_value' => $config['fmc_title'] ?? $this->t('Find'),
+    ];
+
+    $form['fmc']['fmc_url'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'node',
+      '#title' => $this->t('Url'),
+      '#description' => $this->t('Internal or external urls are allowed.'),
+      '#required' => TRUE,
+      '#process_default_value' => FALSE,
+      '#default_value' => $config['fmc_url'] ?? '',
+      '#attributes' => [
+        'data-autocomplete-first-character-blacklist' => '/#?',
+      ],
+      '#element_validate' => [
+        [
+          'Drupal\link\Plugin\Field\FieldWidget\LinkWidget',
+          'validateUriElement',
+        ],
+      ],
+    ];
 
     return $form;
   }
@@ -123,8 +214,22 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
   /**
    * {@inheritdoc}
    */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->configuration['description'] = $values['description'];
+    $this->configuration['view_all_title'] = $values['view_all']['view_all_title'];
+    $this->configuration['view_all_url'] = $values['view_all']['view_all_url'];
+    $this->configuration['fmc_title'] = $values['fmc']['fmc_title'];
+    $this->configuration['fmc_url'] = $values['fmc']['fmc_url'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function build() {
-    $form = [];
+    $config = $this->getConfiguration();
+
+    $build = [];
 
     // This outputs on the admin side a simple summary of the embedded block.
     // Without this, it would try to render the whole block on the node admin.
@@ -132,9 +237,74 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
       return $this->getAdminBuild();
     }
 
-    //$form = \Drupal::formBuilder()->getForm('Drupal\jcc_elevated_embeds\Form\JccElevatedEmbedsZipCityCountySearchForm');
+    $build['library'] = [
+      '#markup' => FALSE,
+      '#attached' => [
+        'library' => ['jcc_elevated_embeds/jcc_appellate_zcs'],
+      ],
+    ];
 
-    return $form;
+    if ($config['description']) {
+      $build['description'] = [
+        '#prefix' => '<p>',
+        '#markup' => $config['description'],
+        '#suffix' => '</p>',
+      ];
+    }
+
+    $build['form']['input'] = [
+      '#type' => 'textfield',
+      '#title' => 'Enter Zip Code, City or County',
+      '#autocomplete_path' => '/admin/config/jcc-elevated/embeds/autocomplete/zip_city_county_info',
+      '#attributes' => [
+        'id' => 'zcs-input',
+      ],
+      '#suffix' => '<div class="block__jcc-appellate-zcs__form__matches" id="zcs-matches"></div>',
+    ];
+
+    $build['form']['actions'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => ['block__jcc-appellate-zcs__form__actions'],
+      ],
+    ];
+
+    if ($config['view_all_url']) {
+      $build['form']['actions']['view_all_link'] = [
+        '#type' => 'link',
+        '#title' => $config['view_all_title'] ?? $this->t('See all Courts'),
+        '#url' => Url::fromUri($config['view_all_url']),
+        '#attributes' => [
+          'class' => [
+            'button button--primary button--normal',
+            'block__jcc-appellate-zcs__form__view-all',
+          ],
+        ],
+      ];
+    }
+
+    if ($config['fmc_url']) {
+      $build['form']['actions']['find_my_court_link'] = [
+        '#type' => 'link',
+        '#title' => $config['fmc_title'] ?? $this->t('Find'),
+        '#url' => Url::fromUri($config['fmc_url']),
+        '#attributes' => [
+          'class' => [
+            'button button--secondary button--normal',
+            'block__jcc-appellate-zcs__form__submit',
+          ],
+        ],
+      ];
+    }
+
+    $build['map'] = [
+      '#type' => 'inline_template',
+      '#template' => '<?xml-stylesheet type="text/css" href="jcc-appellate-zcs.css" ?>' . $this->getMap(),
+    ];
+
+    $build['map_links'] = $this->getMapLinks();
+
+    return $build;
   }
 
   /**
@@ -161,24 +331,39 @@ class JccAppellateZipCitySearchBlock extends BlockBase implements ContainerFacto
   /**
    * {@inheritdoc}
    */
-  protected function getFullDistrictInformation(): array {
-    $cacheId = 'jcc_elevated_embeds:appellate_map_full_district_info';
-
-    if ($cache = $this->cache->get($cacheId)) {
-      return $cache->data;
-    }
-
-    $url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSyg7Rvs4sYh7zEDx0jv0HH9aUTw2aLarcULPeI2UYrxsZALv45MBYb5rvQ_h3s8fxXp43uiGpOhfcG/pub?gid=0&single=true&output=csv';
-    $data = array_map('str_getcsv', file($url));
-    // Remove first line, which is just header info.
-    $headers = array_shift($data);
-    $info = [];
-    foreach ($data as $id => $row) {
-      $info[$id] = array_combine($headers, $row);
-    }
-
-    $this->cache->set($cacheId, $info, CacheBackendInterface::CACHE_PERMANENT);
-
-    return $info;
+  protected function getMap() {
+    $path = $this->moduleHandler->getModule('jcc_elevated_embeds')->getPath();
+    return file_get_contents($path . '/assets/appellate_map/svgs/JCC_California_Districts.svg');
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getMapLinks() {
+    $sections = $this->jccSectionService->getSections();
+
+    $links = [];
+    foreach ($sections as $section) {
+      $section_homepage_nid = $section->jcc_section_homepage->target_id ?? FALSE;
+      $section_machine_name = $section->jcc_section_machine_name->value ?? '';
+
+      // Adding this string check just in case another section is added
+      // that is not part of the District list (like a Division or Subgroup).
+      if ($section_homepage_nid && str_contains($section_machine_name, 'district_')) {
+        $options = [
+          'attributes' => [
+            'class' => ['zcs-map-link__item', $section_machine_name],
+            'name' => ucfirst($section_machine_name),
+          ],
+        ];
+        $url = Url::fromRoute('entity.node.canonical', ['node' => $section_homepage_nid], $options);
+        $links[] = [
+          '#markup' => Link::fromTextAndUrl($section->label(), $url)->toString(),
+        ];
+      }
+    }
+
+    return $links;
+  }
+
 }
