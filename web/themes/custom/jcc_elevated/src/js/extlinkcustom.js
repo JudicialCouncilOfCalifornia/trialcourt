02d58dde3bcc3db936a75971b89999a4d053be89
Archana <archana.ramalingam@jud.ca.gov>
2024-01-24 23:02:31 -0800

    JS fixes

--- web/themes/custom/jcc_elevated/src/js/extlinkcustom.js
+++ web/themes/custom/jcc_elevated/src/js/extlinkcustom.js
@@ -1,18 +1,19 @@
 /**
-* Nav tabs enhancement from seven theme.
+* Ext link icon customization.
 **/
 
 (function ($, Drupal) {
     Drupal.behaviors.extlinkcustom = {
-      attach: function attach(context, settings) {       
-        var exticon = $(context).find('svg.ext');   
-        if(exticon.length > 0){
-          var parent = exticon.parent('a.button');
-          if(parent.length > 0) {           
-            parent.addClass('hide-ext');
-          }
-        }    
-
-      }
-    };
-  })(jQuery, Drupal);
+      attach: function attach(context, drupalSettings) {
+          $(document).ready(function () {
+           var exticon = $(document).find('svg.ext');
+           if(exticon.length > 0) {
+             var parent = exticon.parent('a.button');
+             if(parent.length > 0) {
+               parent.addClass('hide-ext');
+             }
+            }
+          });   
+      }      
+    }
+})(jQuery, Drupal);
