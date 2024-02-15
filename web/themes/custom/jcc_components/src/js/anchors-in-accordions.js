fe7f24347c2bc812408a1a8d395838e788f16bf8
Aditya R Joshi <aditya.joshi-t@jud.ca.gov>
2024-02-15 08:54:50 -0800

    Added feature which keep the default behaviour of accordion(collapse/expand) with jump link.

--- web/themes/custom/jcc_components/src/js/anchors-in-accordions.js
+++ web/themes/custom/jcc_components/src/js/anchors-in-accordions.js
@@ -14,4 +14,22 @@
       });
     }
   };
-  })(jQuery, Drupal);
+  
+  // Attach a click event listener to all anchor tags within 'section' elements.
+  $(document).on('click', 'section a', function(event) {  
+    // Retrieve the href attribute of the clicked link, which is the ID of the target element.
+    var targetId = $(this).attr('href'); 
+    // Determine if the target element is within an accordion by checking if any of its parent divs has a class that includes 'usa-accordion'.
+    var isInAccordion = $(targetId).closest("div").attr("class").includes("usa-accordion");   
+    // If the target is within an accordion, find the ID of the closest accordion container.
+    var accordionContainerId = isInAccordion ? $(targetId).closest("div").attr("id") : null;     
+    // If the target element is within an accordion, expand the accordion section.
+    if (isInAccordion) {
+        // Find the button that controls the accordion section and set 'aria-expanded' to true, indicating the section is expanded.
+        $(targetId).closest("div").prev().children("button").attr('aria-expanded', true);
+        // Remove the 'hidden' property from the accordion content to show it.
+        $(targetId).closest("div").prop('hidden', false);
+    }
+  });
+})(jQuery, Drupal);
+
