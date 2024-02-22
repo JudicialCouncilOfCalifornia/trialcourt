c4890751506f3a14907fe679fc5d096a3552fe07
Aditya R Joshi <aditya.joshi-t@jud.ca.gov>
2024-02-22 10:02:03 -0800

    Implemented the code review , revised the code to implement the accordion function logic.

--- web/themes/custom/jcc_components/src/js/anchors-in-accordions.js
+++ web/themes/custom/jcc_components/src/js/anchors-in-accordions.js
@@ -7,29 +7,15 @@
     attach: function attach(context, settings) {
       $('main').find("a[href*='#']").click(function(e){
         var elemId = $(this).attr('href');
-        elemId = elemId.replace('#','');
-        if ($('#' + elemId).parents('.usa-accordion__content').attr('hidden')) {
-          $('#' + elemId).parents('.usa-accordion__content').removeAttr('hidden');
+        //If accordion is closed
+        if ($(elemId).parents('.usa-accordion__content').prop('hidden')){
+          // Set the accordion body visible
+          $(elemId).parents('.usa-accordion__content').prop('hidden', false)
+          // Set the accordion header to show "-" sign for collapse
+          $(elemId).parents('.usa-accordion__content').prev().children("button").attr('aria-expanded', true);
         }
       });
     }
   };
-  
-  // Attach a click event listener to all anchor tags within 'section' elements.
-  $(document).on('click', 'section a', function(event) {  
-    // Retrieve the href attribute of the clicked link, which is the ID of the target element.
-    var targetId = $(this).attr('href'); 
-    // Determine if the target element is within an accordion by checking if any of its parent divs has a class that includes 'usa-accordion'.
-    var isInAccordion = $(targetId).closest("div").attr("class").includes("usa-accordion");   
-    // If the target is within an accordion, find the ID of the closest accordion container.
-    var accordionContainerId = isInAccordion ? $(targetId).closest("div").attr("id") : null;     
-    // If the target element is within an accordion, expand the accordion section.
-    if (isInAccordion) {
-        // Find the button that controls the accordion section and set 'aria-expanded' to true, indicating the section is expanded.
-        $(targetId).closest("div").prev().children("button").attr('aria-expanded', true);
-        // Remove the 'hidden' property from the accordion content to show it.
-        $(targetId).closest("div").prop('hidden', false);
-    }
-  });
 })(jQuery, Drupal);
 
