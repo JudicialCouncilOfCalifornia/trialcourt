3f3e1cde464a94d49f514573a8a13aea76368c0d
Mel Wong <mel.wong@jud.ca.gov>
2024-04-12 14:57:39 -0700

    COURTS-21: citing link improvement & missed review granted display

--- web/modules/custom/jcc_custom/src/Plugin/migrate/process/JccMediaLookup.php
+++ web/modules/custom/jcc_custom/src/Plugin/migrate/process/JccMediaLookup.php
@@ -2,16 +2,17 @@
 
 namespace Drupal\jcc_custom\Plugin\migrate\process;
 
+use Drupal\media\Entity\Media;
 use Drupal\migrate\MigrateException;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
 /**
- * Creates media path from a provided mid.
+ * Returns media information from the provided mid.
  *
  * @MigrateProcessPlugin(
- *   id = "jcc_media_path"
+ *   id = "jcc_media_lookup"
  * )
  *
  * To do custom value transformations use the following.
@@ -19,19 +20,26 @@ use Drupal\migrate\Row;
  *
  * @code
  * field_link:
- *   plugin: jcc_media_path
- *   source: string
+ *   plugin: jcc_media_lookup
+ *   source: array
  * @endcode
  */
-class JccMediaPath extends ProcessPluginBase {
+class JccMediaLookup extends ProcessPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     if ($value && is_numeric($value)) {
-      $uri = '/media/' . $value;
-      return $uri;
+      $link = [];
+      $media = Media::load($value);
+      if ($media) {
+        $link = [
+          'uri' => 'entity:media/' . $value,
+          'description' => $media->get('field_media_text')->value,
+        ];
+      }
+      return $link;
     }
     else {
       throw new MigrateException(sprintf('"%s" is not a valid media id.', $value));
