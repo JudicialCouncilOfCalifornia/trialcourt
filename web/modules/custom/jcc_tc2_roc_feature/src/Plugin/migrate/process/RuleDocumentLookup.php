3ece4d623cea6fa058dc1864957e2828bfc7c5f9
Philip Boden <phil@chapterthreellc.com>
2024-05-06 21:38:28 -0700

    [COURTS-11] -- Allow for nested paragraph information migration. CKeditor changes for ROC

--- web/modules/custom/jcc_tc2_roc_feature/src/Plugin/migrate/process/RuleDocumentLookup.php
+++ web/modules/custom/jcc_tc2_roc_feature/src/Plugin/migrate/process/RuleDocumentLookup.php
@@ -20,7 +20,7 @@ class RuleDocumentLookup extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($source, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $fields = ['destid1', 'destid2'];
+    $fields = ['destid1', 'destid2', 'sourceid3'];
     $paragraphs = \Drupal::database()->select('migrate_map_' . $this->configuration['migration_id'], 't')
       ->fields('t', $fields)
       ->condition('t.sourceid2', $source)
@@ -36,14 +36,33 @@ class RuleDocumentLookup extends ProcessPluginBase {
       if ($row->destid1) {
         $paragraph = Paragraph::load($row->destid1);
         if ($paragraph) {
+
+          // Try to get the UUID of the parent paragraph that the section should
+          // go into.
+          $parent_paragraph_uuids = [];
+          if ($row->sourceid3) {
+            $parent_fields = ['destid1', 'destid2'];
+            $parent_paragraphs = \Drupal::database()->select('migrate_map_' . $this->configuration['migration_id'], 't')
+              ->fields('t', $parent_fields)
+              ->condition('t.sourceid1', $row->sourceid3)
+              ->execute();
+            foreach ($parent_paragraphs as $parent_row) {
+              $parent_paragraph = Paragraph::load($parent_row->destid1);
+              if ($parent_paragraph) {
+                $parent_paragraph_uuids[] = $parent_paragraph->uuid();
+              }
+            }
+          }
+
           $settings = [
             'layout' => 'roc_section_listing',
             'config' => [
               'label' => '',
             ],
-            'parent_uuid' => '',
+            'parent_uuid' => !empty($parent_paragraph_uuids[0]) ? $parent_paragraph_uuids[0] : '',
             'region' => 'content',
           ];
+
           $paragraph->setBehaviorSettings('layout_paragraphs', $settings);
           $paragraph->save();
         }
