6b898bdc7a85c01060cf55b2444fb3ae494c4f62
ravitejayarlagadda <Raviteja.Yarlagadda-T@jud.ca.gov>
2024-08-12 15:24:29 -0400

    removed duplicate contracosta name

--- .circleci/scripts/project-cc.sh
+++
@@ -1,7 +0,0 @@
-# Pantheon Project UUID.
-UUID=81f168b4-d5d0-4016-896b-bfdd7f976372
-SITE_CODE=cc
-# If false, pantheon will deploy to dev. If true it will get the Live tag.
-LIVE=true
-# If false, deployments will be skipped for the master branch for this site.
-DEPLOY_MASTER=true
