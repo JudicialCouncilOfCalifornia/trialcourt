0f49f6413112c27e8d765369fb11d7ba379b63b5
ravitejayarlagadda <Raviteja.Yarlagadda-T@jud.ca.gov>
2024-08-12 14:37:42 -0400

    Added contra costa instance

---
+++ .circleci/scripts/project-contracosta.sh
@@ -0,0 +1,7 @@
+# Pantheon Project UUID.
+UUID=86d0c324-a66c-4a61-8042-ab61ac27bcc8
+SITE_CODE=contracosta
+# If false, pantheon will deploy to dev. If true it will get the Live tag.
+LIVE=true
+# If false, deployments will be skipped for the master branch for this site.
+DEPLOY_MASTER=true
