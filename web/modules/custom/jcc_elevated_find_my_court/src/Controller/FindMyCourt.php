b52bce5bf9e7d4959979d551ce4e7e7a2418ae89
baptisten <baptiste.niviere@gmail.com>
2024-10-08 14:53:42 -0700

    templating

--- web/modules/custom/jcc_elevated_find_my_court/src/Controller/FindMyCourt.php
+++ web/modules/custom/jcc_elevated_find_my_court/src/Controller/FindMyCourt.php
@@ -15,14 +15,29 @@ class FindMyCourt extends ControllerBase {
    * @return array
    *   A simple renderable array.
    */
-  public function findMyCourt() {
-    return [
-      '#type' => 'inline_template',
-      '#template' => '<pre>test: {{zip}}</pre><div class="section__content box"><div class="container stack"><iframe src="{{ url }}" id="iframe-ftrc" class="iframe-ftrc" title="County courthouses found by zip code or city" style="height: 2390.37px;width:100%;"></iframe></div></div>',
-      '#context' => [
-        'url' => 'modules/custom/jcc_elevated_find_my_court/src/ftrc/index.html?query=94117',
-      ],
+  public function findMyCourt($zip) {
+    $output = [
+      '#markup' => $this->t('<div class="section__content box">
+        <div class="container stack">
+            <form onsubmit="this.action = \'/find-my-court/\' + this.urlarg.value;">
+                <div class="cluster center">
+                    <div class="form-item form-item-text-field-label placeholder-container js-form-type-textfield">
+			                  <input data-drupal-selector="edit-text-field-label" aria-describedby="edit-text-field-label--description" type="text" id="edit-text-field-label" name="urlarg" id="urlarg" value="" size="60" maxlength="255" class="form-text" placeholder="@zip" ">
+                        <label for="edit-text-field-label">Zipcode</label>
+                    </div>
+                    <button class="button button--text button--normal icon--arrow" type="submit">
+					              <span class="usa-sr-only">Search</span>
+				            </button>
+                </div>
+            </form>
+         </div>
+         <div class="container stack">
+            <iframe src="/modules/custom/jcc_elevated_find_my_court/src/ftrc/index.html?query=@zip" id="iframe-ftrc" class="iframe-ftrc" title="County courthouses found by zip code or city" style="height: 2390.37px;width:100%;" frameBorder="0"></iframe>
+         </div>
+        </div>', ['@zip' => $zip]),
     ];
+
+    return $output;
   }
 
 }
