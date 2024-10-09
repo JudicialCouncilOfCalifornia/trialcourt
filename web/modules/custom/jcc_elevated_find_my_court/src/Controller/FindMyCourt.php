e9b80ed6ef3f188855bbd1969a1a24e397339cc4
baptisten <baptiste.niviere@gmail.com>
2024-10-09 10:21:41 -0700

    paragraph and template

--- web/modules/custom/jcc_elevated_find_my_court/src/Controller/FindMyCourt.php
+++ web/modules/custom/jcc_elevated_find_my_court/src/Controller/FindMyCourt.php
@@ -17,26 +17,67 @@ class FindMyCourt extends ControllerBase {
    */
   public function findMyCourt($zip) {
     $output = [
-      '#markup' => $this->t('<div class="section__content box">
-        <div class="container stack">
-            <form onsubmit="this.action = \'/find-my-court/\' + this.urlarg.value;">
-                <div class="cluster center">
-                    <div class="form-item form-item-text-field-label placeholder-container js-form-type-textfield">
-			                  <input data-drupal-selector="edit-text-field-label" aria-describedby="edit-text-field-label--description" type="text" id="edit-text-field-label" name="urlarg" id="urlarg" value="" size="60" maxlength="255" class="form-text" placeholder="@zip" ">
-                        <label for="edit-text-field-label">Zipcode</label>
+      '#markup' => $this->t('<div>
+        <script type="text/javascript">
+          document.addEventListener("DOMContentLoaded", function() {
+            let iframe = document.getElementsByTagName("iframe")[0];
+            if (iframe) {
+              let iframeID = \'#\' + document.getElementsByTagName("iframe")[0].id;
+              iFrameResize({lowestElement: true}, iframeID);
+            }
+          });
+        </script>
+        <div class="hangover hangover--solid-primary-dark-x" style="--hangover-offset: -22ch;">
+          <div class="hangover__top">
+            <div class="box">
+              <div class="with-sidebar detect-wrap detect-wrap--observed container">
+                <div class="hangover__top-content">
+                  <div class="stack">
+                    <div class="stack">
+                      <h1 class="hangover__title stack__sm-space">Find my court</h1>
                     </div>
-                    <button class="button button--text button--normal icon--arrow" type="submit">
-					              <span class="usa-sr-only">Search</span>
-				            </button>
+                  </div>
                 </div>
-            </form>
-         </div>
-         <div class="container stack">
-            <iframe src="/modules/custom/jcc_elevated_find_my_court/src/ftrc/index.html?query=@zip" id="iframe-ftrc" class="iframe-ftrc" title="County courthouses found by zip code or city" style="height: 2390.37px;width:100%;" frameBorder="0"></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="section box"></div>
+          <div class="container stack">
+            <div class="inline-form inline-form--default">
+              <form class="views-exposed-form bef-exposed-form" onsubmit="this.action = \'/find-my-court/\' + this.urlarg.value;">
+                <div class="form-item">
+                  <input
+                    class="form-text"
+                    placeholder="Enter a city or a zip code"
+                    type="text"
+                    value="@zip"
+                    name="urlarg"
+                    id="urlarg"
+                  />
+                </div>
+                <div class="form-actions form-wrapper" id="edit-actions">
+                  <button class=\'button form-submit\' type=\'submit\'>
+                    <span class=\'inline-form__submit-line\'></span>
+                    <span class=\'inline-form__submit-circle\'></span>
+                    <span class=\'sr-only\'>Search</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="container stack">
+            <div class="box">
+             <iframe src="/modules/custom/jcc_elevated_find_my_court/src/ftrc/index.html?query=@zip" id="iframe-ftrc" class="iframe-ftrc" title="County courthouses found by zip code or city" style="height: 2390.37px;width:100%;" frameBorder="0"></iframe>
+            </div>
+          </div>
          </div>
         </div>', ['@zip' => $zip]),
     ];
 
+    $output['#attached']['library'][] = 'jcc_elevated_find_my_court/storybook-inlineform';
+    $output['#attached']['library'][] = 'jcc_elevated_find_my_court/storybook-herohangover';
+    $output['#attached']['library'][] = 'jcc_elevated_find_my_court/iframeresizer';
     return $output;
   }
 
