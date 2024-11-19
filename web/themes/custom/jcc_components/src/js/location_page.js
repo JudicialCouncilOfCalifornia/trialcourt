5e8039f3eb10769021323c9b20a8f8a2536a5b8f
Aditya R Joshi <aditya.joshi-t@jud.ca.gov>
2024-11-19 15:54:08 -0800

    Added config files with new changes in label and select/Deselect case served fields.

--- web/themes/custom/jcc_components/src/js/location_page.js
+++ web/themes/custom/jcc_components/src/js/location_page.js
@@ -1,21 +1,12 @@
 (function ($, Drupal) {
-    console.log("me");
-    console.log($(this))
     $(".form-checkbox").change(function(){
-        console.log("something change");
         if($(this).is(':checked')){
-               console.log($(this).val())
-            console.log("this");
-            console.log($(this).next().text().trim())
             var checked = $(this).next().text().trim()
             if(checked.toLowerCase().includes("all cases")){
                 const $parentContainer = $(this).closest('#edit-field-cases-served-wrapper');
                 $parentContainer.find('.option').prop('checked', this.checked);
-            }else{
-                const $parentContainer = $(this).closest('#edit-field-cases-served-wrapper');
-                $parentContainer.find('.option').prop('checked', this.checked);
-            }                   
+            }                  
         } 
     })
 
-})(jQuery, Drupal);
+})(jQuery, Drupal);3
