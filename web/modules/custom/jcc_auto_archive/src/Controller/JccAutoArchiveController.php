43ee1d749beead5d9dbe9508c58e48cb5432d769
Alex Wu <Alex.Wu@jud.ca.gov>
2025-01-07 10:55:59 -0800

    improved the styling of the code

--- web/modules/custom/jcc_auto_archive/src/Controller/JccAutoArchiveController.php
+++ web/modules/custom/jcc_auto_archive/src/Controller/JccAutoArchiveController.php
@@ -14,27 +14,23 @@ class JccAutoArchiveController extends ControllerBase {
    * /
    */
   public function autoArchive_cron() {
-
     $five_years_ago = strtotime('-5 years');
     $query = \Drupal::entityQuery('node')
-        ->condition('type', 'news')
-        ->condition('field_news_type.entity:taxonomy_term.name', 'NewsLink')
-        ->condition('created', $five_years_ago, '<')
-        ->condition('status', 1); // Only published content
+      ->condition('type', 'news')
+      ->condition('field_news_type.entity:taxonomy_term.name', 'NewsLink')
+      ->condition('created', $five_years_ago, '<')
+      ->condition('status', 1); // Only published content
 
     $nids = $query->execute();
     if (!empty($nids)) {
-        foreach ($nids as $nid) {
-
-            \Drupal::logger('jcc_newslinks')->info("awu to change the state of ".$nid );
-            $node = \Drupal\node\Entity\Node::load($nid);
-            $node->set('moderation_state', 'archived'); // Set your custom state
-
-            $node->save();
-            \Drupal::logger('jcc_newslinks')->info("awu saved as archived for ".$nid );
-        }
+      foreach ($nids as $nid) {
+        \Drupal::logger('jcc_newslinks')->info("awu to change the state of ".$nid );
+        $node = \Drupal\node\Entity\Node::load($nid);
+        $node->set('moderation_state', 'archived'); // Set your custom state
+        $node->save();
+        \Drupal::logger('jcc_newslinks')->info("awu saved as archived for ".$nid );
+      }
     }
-
     \Drupal::logger('jcc_newslinks')->info("awu exiting autoArchive_cron()");
   }
 
