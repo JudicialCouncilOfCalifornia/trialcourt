395829d4f66dd831e64b0867b5e0059c6bc86632
Alex Wu <Alex.Wu@jud.ca.gov>
2025-04-08 13:31:50 -0700

    feature/new35 added the base line for the dynamic sort module

---
+++ web/modules/custom/jcc_dynamic_sort/src/EventSubscriber/SearchRequestSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\jcc_dynamic_sort\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+
+/**
+ * Subscriber to hide Alert nodes from anonymous visitors.
+ */
+class SearchRequestSubscriber implements EventSubscriberInterface {
+
+  protected $logger;
+
+  public function __construct(LoggerChannelFactoryInterface $logger_factory) {
+    $this->logger = $logger_factory->get('dynamic_sort');
+  }
+
+  public function onKernelRequest(RequestEvent $event) {
+    $request = $event->getRequest();
+    $path = $request->getPathInfo();
+
+    // Check if it's the /search path.
+    if ($path === '/search') {
+      $keywords = $request->query->get('keywords');
+      $this->logger->info('Search triggered with keywords: @keywords', ['@keywords' => $keywords]);
+
+      // Insert your custom logic here. For example:
+      // - Trigger batch processing
+      // - Set a timestamp
+      // - Update entity values (though saving entities on GET is not ideal)
+    }
+  }
+
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+    ];
+  }
+
+}
