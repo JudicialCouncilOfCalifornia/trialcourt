eeec2498a2b4dc9eba960cbdd3c85e8f51ec4b6e
baptisten <baptiste.niviere@gmail.com>
2026-02-18 15:55:58 -0800

    new module

---
+++ web/modules/custom/jcc_pdf_upload_validation_checker/src/Form/SettingsForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\jcc_pdf_upload_validation_checker\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure JCC PDF upload validation checker settings for this site.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'jcc_pdf_upload_validation_checker_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['jcc_pdf_upload_validation_checker.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('jcc_pdf_upload_validation_checker.settings');
+
+    $form['pdf_validation_api'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Api to use for validation'),
+      '#options' => [
+        'PDF audit' => $this->t('pdf_audit'),
+        'EqualWeb' => $this->t('equal_web'),
+      ],
+      '#default_value' => $config->get('pdf_validation_api') ?? 'pdf_audit',
+      '#required' => TRUE,
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getValue('pdf_validation_api') != 'PDF audit' && $form_state->getValue('pdf_validation_api') != 'EqualWeb') {
+      $form_state->setErrorByName('pdf_validation_api', $this->t('The value is not correct.'));
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('jcc_pdf_upload_validation_checker.settings')
+      ->set('pdf_validation_api', $form_state->getValue('pdf_validation_api'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
