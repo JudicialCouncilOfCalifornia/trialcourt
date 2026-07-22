443e53abc077fb6520bcaa183812296eef7e1fdc
Mel Wong <mel.wong@jud.ca.gov>
2026-07-22 09:26:15 -0700

    ADA-304: announce iframe updates to screen readers

--- web/themes/custom/jcc_elevated/src/js/view-ajax-customizations.js
+++ web/themes/custom/jcc_elevated/src/js/view-ajax-customizations.js
@@ -1,16 +1,28 @@
 (function (Drupal) {
   Drupal.behaviors.viewAjaxCustomizations = {
-    attach: function (context, settings) {
-      if (context !== document) {
-        // Announce view region update occurrence.
-        const resultsView = document.querySelectorAll('.view-results');
-        const resultsCount = resultsView[0].querySelector('.cluster .views-results_content-header').textContent;
-        let message = Drupal.t('The view has been updated.');
-        if (resultsCount) {
-          message = Drupal.t('The view has been updated. Now showing @count.', { '@count': resultsCount.trim()});
+    attach: function (context, drupalSettings) {
+      function docReady(fn) {
+        // See if DOM is already available.
+        if (document.readyState === 'complete' || document.readyState === 'interactive') {
+          // Call on next available tick.
+          setTimeout(fn, 1);
+        } else {
+          document.addEventListener('DOMContentLoaded', fn);
         }
-        Drupal.announce(message, 'assertive');
       }
+
+      docReady(function() {
+        // Announce view region update occurrence.
+        if (context !== document) {
+          const resultsView = document.querySelectorAll('.view-results');
+          const resultsCount = resultsView[0].querySelector('.cluster .views-results_content-header').textContent;
+          let message = Drupal.t('The view has been updated.');
+          if (resultsCount) {
+            message = Drupal.t('The view has been updated. Now showing @count.', { '@count': resultsCount.trim()});
+          }
+          Drupal.announce(message, 'assertive');
+        }
+      });
     }
   };
 })(Drupal);
