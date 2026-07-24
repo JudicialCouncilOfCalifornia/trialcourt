e60676f3756c753799da4d77d57b56ccb3e8001a
Rolando Payán Mosqueda <rolando.payan@kanopi.com>
2026-07-24 14:24:52 -0400

    tw30086830 D9-to-D10 upgrade for Inyo County

--- web/themes/custom/jcc_components/src/js/nav-tabs.js
+++ web/themes/custom/jcc_components/src/js/nav-tabs.js
@@ -1,45 +1,37 @@
 /**
-* Nav tabs enhancement from seven theme.
-**/
+ * Nav tabs enhancement from seven theme.
+ */
 
-(function ($, Drupal) {
-    function init(i, tab) {
-      var $tab = $(tab);
-      var $target = $tab.find('[data-drupal-nav-tabs-target]');
-      var isCollapsible = $tab.hasClass('is-collapsible');
-  
-      function openMenu(e) {
-        $target.toggleClass('is-open');
-      }
-  
-      function handleResize(e) {
-        $tab.addClass('is-horizontal');
-        var $tabs = $tab.find('.tabs');
-        var isHorizontal = $tabs.outerHeight() <= $tabs.find('.tabs__tab').outerHeight();
-        $tab.toggleClass('is-horizontal', isHorizontal);
-        if (isCollapsible) {
-          $tab.toggleClass('is-collapse-enabled', !isHorizontal);
-        }
-        if (isHorizontal) {
-          $target.removeClass('is-open');
-        }
-      }
-  
-      $tab.addClass('position-container is-horizontal-enabled');
-  
-      $tab.on('click.tabs', '[data-drupal-nav-tabs-trigger]', openMenu);
-      $(window).on('resize.tabs', Drupal.debounce(handleResize, 150)).trigger('resize.tabs');
+(function ($, once, Drupal) {
+  function init(tab) {
+    const $tab = $(tab);
+    const $target = $tab.find('[data-drupal-nav-tabs-target]');
+    const isCollapsible = $tab.hasClass('is-collapsible');
+    function openMenu(e) {
+      $target.toggleClass('is-open');
     }
-  
-    Drupal.behaviors.navTabs = {
-      attach: function attach(context, settings) {
-        var $tabs = $(context).find('[data-drupal-nav-tabs]');
-        if ($tabs.length) {
-          var notSmartPhone = window.matchMedia('(min-width: 300px)');
-          if (notSmartPhone.matches) {
-            $tabs.once('nav-tabs').each(init);
-          }
-        }
+    function handleResize(e) {
+      $tab.addClass('is-horizontal');
+      const $tabs = $tab.find('.tabs');
+      const isHorizontal = $tabs.outerHeight() <= $tabs.find('.tabs__tab').outerHeight();
+      $tab.toggleClass('is-horizontal', isHorizontal);
+      if (isCollapsible) {
+        $tab.toggleClass('is-collapse-enabled', !isHorizontal);
       }
-    };
-  })(jQuery, Drupal);
+      if (isHorizontal) {
+        $target.removeClass('is-open');
+      }
+    }
+    $tab.addClass('position-container is-horizontal-enabled');
+    $tab.on('click.tabs', '[data-drupal-nav-tabs-trigger]', openMenu);
+    $(window).on('resize.tabs', Drupal.debounce(handleResize, 150)).trigger('resize.tabs');
+  }
+  Drupal.behaviors.navTabs = {
+    attach: function attach(context, settings) {
+      const notSmartPhone = window.matchMedia('(min-width: 300px)');
+      if (notSmartPhone.matches) {
+        once('nav-tabs', '[data-drupal-nav-tabs]', context).forEach(init);
+      }
+    }
+  };
+})(jQuery, once, Drupal);
