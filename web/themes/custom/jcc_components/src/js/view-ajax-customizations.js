eddb48093045df6c49e1a49a758ffac76d1bfd8a
Mel Wong <mel.wong@jud.ca.gov>
2026-07-31 16:22:13 -0700

    ADA-350: drupal announce integration for view ajax

--- web/themes/custom/jcc_components/src/js/view-ajax-customizations.js
+++ web/themes/custom/jcc_components/src/js/view-ajax-customizations.js
@@ -6,9 +6,12 @@
         // Announce view update occurrence.
         let message = Drupal.t('The view has been updated.');
         // Check if the view has a results count.
-        const resultsView = context.querySelectorAll('.view-results');
+        const resultsViews = [
+          '.jcc-news-listing__content',
+        ];
+        const resultsView = context.querySelectorAll(resultsViews);
         if (resultsView.length > 0) {
-          const resultsCount = resultsView[0].querySelector('.cluster .views-results_content-header').textContent;
+          const resultsCount = resultsView[0].querySelector('.jcc-listing_result').textContent;
           if (resultsCount) {
             message = message + Drupal.t(' Now showing @count.', { '@count': resultsCount.trim()});
           }
