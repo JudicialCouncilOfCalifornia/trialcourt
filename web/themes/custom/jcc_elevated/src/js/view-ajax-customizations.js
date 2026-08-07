09771684a065d7e19621a001a4cb40d48e791dec
Mel Wong <mel.wong@jud.ca.gov>
2026-08-06 20:24:27 -0700

    ADA-304: revert elevated view ajax edits

--- web/themes/custom/jcc_elevated/src/js/view-ajax-customizations.js
+++ web/themes/custom/jcc_elevated/src/js/view-ajax-customizations.js
@@ -1,28 +1,16 @@
 (function (Drupal) {
   Drupal.behaviors.viewAjaxCustomizations = {
-    attach: function (context, drupalSettings) {
-      function docReady(fn) {
-        // See if DOM is already available.
-        if (document.readyState === 'complete' || document.readyState === 'interactive') {
-          // Call on next available tick.
-          setTimeout(fn, 1);
-        } else {
-          document.addEventListener('DOMContentLoaded', fn);
-        }
-      }
-
-      docReady(function() {
+    attach: function (context, settings) {
+      if (context !== document) {
         // Announce view region update occurrence.
-        if (context !== document) {
-          const resultsView = document.querySelectorAll('.view-results');
-          const resultsCount = resultsView[0].querySelector('.cluster .views-results_content-header').textContent;
-          let message = Drupal.t('The view has been updated.');
-          if (resultsCount) {
-            message = Drupal.t('The view has been updated. Now showing @count.', { '@count': resultsCount.trim()});
-          }
-          Drupal.announce(message, 'assertive');
+        const resultsView = document.querySelectorAll('.view-results');
+        const resultsCount = resultsView[0].querySelector('.cluster .views-results_content-header').textContent;
+        let message = Drupal.t('The view has been updated.');
+        if (resultsCount) {
+          message = Drupal.t('The view has been updated. Now showing @count.', { '@count': resultsCount.trim()});
         }
-      });
+        Drupal.announce(message, 'assertive');
+      }
     }
   };
 })(Drupal);
