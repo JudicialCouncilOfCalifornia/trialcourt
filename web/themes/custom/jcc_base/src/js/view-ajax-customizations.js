aba16dcfee465e6eb28110179673403075ad24bb
Mel Wong <mel.wong@jud.ca.gov>
2026-08-11 12:00:11 -0700

    ADA-321: neglected docready check

--- web/themes/custom/jcc_base/src/js/view-ajax-customizations.js
+++ web/themes/custom/jcc_base/src/js/view-ajax-customizations.js
@@ -38,26 +38,28 @@
         }
       }
       
-      // Listen for event triggered by Views AJAX.
-      if (context !== document) {
-        if (once('jcc-view-ajax-announce', context).length === 0) {
-          return;
-        }
-        // Announce view update occurrence.
-        let message = Drupal.t('The view has been updated.');
-        // Check if the view has a results count.
-        const resultsViews = [
-          '.jcc-news-listing__content',
-        ];
-        const resultsView = context.querySelectorAll(resultsViews);
-        if (resultsView.length > 0) {
-          const resultsCount = resultsView[0].querySelector('.jcc-listing_result').textContent;
-          if (resultsCount) {
-            message = message + Drupal.t(' Now showing @count.', { '@count': resultsCount.trim()});
+      docReady(function() {
+        // Listen for event triggered by Views AJAX.
+        if (context !== document) {
+          if (once('jcc-view-ajax-announce', context).length === 0) {
+            return;
           }
+          // Announce view update occurrence.
+          let message = Drupal.t('The view has been updated.');
+          // Check if the view has a results count.
+          const resultsViews = [
+            '.jcc-news-listing__content',
+          ];
+          const resultsView = context.querySelectorAll(resultsViews);
+          if (resultsView.length > 0) {
+            const resultsCount = resultsView[0].querySelector('.jcc-listing_result').textContent;
+            if (resultsCount) {
+              message = message + Drupal.t(' Now showing @count.', { '@count': resultsCount.trim()});
+            }
+          }
+          announce(message, 'assertive');
         }
-        announce(message, 'assertive');
-      }
+      });
     }
   };
 })(Drupal, once);
