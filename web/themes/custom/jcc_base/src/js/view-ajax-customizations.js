e3e96b5f79c6ed1557ccd2630adc99c827e6b279
Mel Wong <mel.wong@jud.ca.gov>
2026-08-11 13:57:15 -0700

    ADA-321: docready not required for AJAX

--- web/themes/custom/jcc_base/src/js/view-ajax-customizations.js
+++ web/themes/custom/jcc_base/src/js/view-ajax-customizations.js
@@ -25,41 +25,27 @@
           region.textContent = message;
         }, 50);
       }
-
-      function docReady(fn) {
-        console.clear();
-        console.log('View AJAX update detected.');
-        // See if DOM is already available.
-        if (document.readyState === 'complete' || document.readyState === 'interactive') {
-          // Call on next available tick.
-          setTimeout(fn, 1);
-        } else {
-          document.addEventListener('DOMContentLoaded', fn);
+   
+      // Listen for event triggered by Views AJAX.
+      if (context !== document) {
+        if (once('jcc-view-ajax-announce', context).length === 0) {
+          return;
         }
+        // Announce view update occurrence.
+        let message = Drupal.t('The view has been updated.');
+        // Check if the view has a results count.
+        const resultsViews = [
+          '.jcc-news-listing__content',
+        ];
+        const resultsView = context.querySelectorAll(resultsViews);
+        if (resultsView.length > 0) {
+          const resultsCount = resultsView[0].querySelector('.jcc-listing_result').textContent;
+          if (resultsCount) {
+            message = message + Drupal.t(' Now showing @count.', { '@count': resultsCount.trim()});
+          }
+        }
+        announce(message, 'assertive');
       }
-      
-      docReady(function() {
-        // Listen for event triggered by Views AJAX.
-        if (context !== document) {
-          if (once('jcc-view-ajax-announce', context).length === 0) {
-            return;
-          }
-          // Announce view update occurrence.
-          let message = Drupal.t('The view has been updated.');
-          // Check if the view has a results count.
-          const resultsViews = [
-            '.jcc-news-listing__content',
-          ];
-          const resultsView = context.querySelectorAll(resultsViews);
-          if (resultsView.length > 0) {
-            const resultsCount = resultsView[0].querySelector('.jcc-listing_result').textContent;
-            if (resultsCount) {
-              message = message + Drupal.t(' Now showing @count.', { '@count': resultsCount.trim()});
-            }
-          }
-          announce(message, 'assertive');
-        }
-      });
     }
   };
 })(Drupal, once);
